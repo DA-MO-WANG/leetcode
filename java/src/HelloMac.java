c61cf16b1c653cd9885addf9eb58a615781a9bb2
han guo <guohan741@163.com>
2021-08-24 19:10:00 +0800

    autosave leet

--- java/src/HelloMac.java
+++ java/src/HelloMac.java
@@ -1,5 +1,10 @@
 public class HelloMac {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         System.out.println("hello mac 13");
+
+        HelloMac helloMac = new HelloMac();
+        HelloMac clone = (HelloMac)helloMac.clone();
+        System.out.println(clone == helloMac);
+
     }
 }

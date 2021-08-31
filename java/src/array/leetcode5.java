acf261cfcaa302a8d8cf6c42e17652b75ff7e3ab
han guo <guohan741@163.com>
2021-08-31 23:26:00 +0800

    autosave leet

--- java/src/array/leetcode5.java
+++ java/src/array/leetcode5.java
@@ -27,7 +27,7 @@ public class leetcode5 {
             int len = Math.max(len1, len2);
             if (len > end - start) {
                 start = i - (len - 1) / 2;
-                end = i + (len - 1) / 2;
+                end = i + len / 2;
             }
         }
         return s.substring(start, end + 1);

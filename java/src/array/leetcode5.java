bd2aee2e02db4f109dbf6e0280186b4b0c28657e
han guo <guohan741@163.com>
2021-08-31 23:24:00 +0800

    autosave leet

--- java/src/array/leetcode5.java
+++ java/src/array/leetcode5.java
@@ -18,18 +18,26 @@ public class leetcode5 {
 
     public String longestPalindromez(String s) {
         int n = s.length();
+        int start = 0;
+        int end = 0;
         //先拿到回文中心
         for(int i = 0; i < n; i++) {
-
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + (len - 1) / 2;
+            }
         }
-
-
-
+        return s.substring(start, end + 1);
     }
     public int expand(String s, int left, int right) {
         while (left > 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            
+            --left;
+            ++right;
         }
+        return right - left - 1;
     }
 //    int res = 0;
 //        int[] two = new int[2];

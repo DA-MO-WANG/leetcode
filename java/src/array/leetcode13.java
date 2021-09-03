198660e965d80ddfde263caf14896c38d1d47fc9
han guo <guohan741@163.com>
2021-09-03 15:02:00 +0800

    autosave leet

--- java/src/array/leetcode13.java
+++ java/src/array/leetcode13.java
@@ -1,5 +1,20 @@
 package array;
 
 public class leetcode13 {
-    
+    public int romanToInt(String s) {
+        //先考虑没有特殊规则
+        char[] letters = s.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+
+        }
+
+    }
+
+    public int transfer(char a) {
+        switch (a) {
+            case 'I':
+                return 1;
+                case ''
+        }
+    }
 }

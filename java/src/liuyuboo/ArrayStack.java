1a2f1dbacfe9e4ef081c069fd54b54eb9bb0a10b
han guo <guohan741@163.com>
2021-09-03 20:00:00 +0800

    autosave leet

--- java/src/liuyuboo/ArrayStack.java
+++ java/src/liuyuboo/ArrayStack.java
@@ -1,2 +1,33 @@
-package liuyuboo;public class ArrayStack {
+package liuyuboo;
+
+public class ArrayStack<E> implements Stack{
+    E[] table;
+    int size;
+    @Override
+    public void push(Object e) {
+        table[size++] = (E)e;
+    }
+
+    @Override
+    public Object pop() {
+        E del = table[size - 1];
+        table[size--] = null;
+        return del;
+    }
+
+    @Override
+    public Object peek() {
+
+        return table[size];
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 }

8e71a235a2ccd014888ee7575f3b70fafd584a3a
han guo <guohan741@163.com>
2021-09-06 16:36:00 +0800

    autosave leet

--- java/src/hw/zifuchuanfenge.java
+++ java/src/hw/zifuchuanfenge.java
@@ -14,19 +14,19 @@ public class zifuchuanfenge {
                     System.out.print(cr);
                 }
                 for(int i = 1; i <= 8 - ch.length; i++) {
-                    System.out.println('0');
+                    System.out.print('0');
                 }
                 System.out.println();
             }else {
                 for (int i = 0; i < ch.length; i++) {
-                    System.out.println(ch[i]);
+                    System.out.print(ch[i]);
                     if ((i + 1) % 8 == 0) {
                         System.out.println();
                     }
                 }
                 if (ch.length % 8 != 0) {
                     for (int i = 1; i <= 8 - ch.length % 8; i++) {
-                        System.out.println('0');
+                        System.out.print('0');
                     }
                     System.out.println();
                 }

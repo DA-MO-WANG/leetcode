aa24e9fe08b925ea6c8d537e7fc3d85cc7a164ba
han guo <guohan741@163.com>
2021-09-06 16:14:00 +0800

    autosave leet

--- java/src/hw/zifuchuanfenge.java
+++ java/src/hw/zifuchuanfenge.java
@@ -1,5 +1,35 @@
 package hw;
 
+import java.util.Scanner;
+
 public class zifuchuanfenge {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] s = new String[100];
+        s[0] = in.nextLine();
+        s[1] = in.nextLine();
+        for (int i = 0; i < 2; i++) {
+            if (s[i].length() == 8) {
+                System.out.println(s[i]);
+            }
+            if (s[i].length() < 8) {
+                System.out.println(s[i]);
+                for (int j = 0; j < 8 - s[i].length(); j++) {
+                    System.out.println("0");
+                }
+                System.out.println();
+            }else {
+                int n = s[i].length() / 8;
+                int m = s[i].length() % 8;
+                if (m == 0) {
+                    for (int k = 0; )
+                }
+            }
+
+
+        }
+
+        System.out.println();
+
+    }
 }

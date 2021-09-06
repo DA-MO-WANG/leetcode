0162b1d9f13edcb4ee5642a6638ea50a088b686a
han guo <guohan741@163.com>
2021-09-06 16:34:01 +0800

    autosave leet

--- java/src/hw/zifuchuanfenge.java
+++ java/src/hw/zifuchuanfenge.java
@@ -38,37 +38,5 @@ public class zifuchuanfenge {
 
 
 
-        String[] s = new String[100];
-        s[0] = in.nextLine();
-        s[1] = in.nextLine();
-        for (int i = 0; i < 2; i++) {
-            if (s[i].length() == 8) {
-                System.out.println(s[i]);
-            }
-            if (s[i].length() < 8) {
-                System.out.println(s[i]);
-                for (int j = 0; j < 8 - s[i].length(); j++) {
-                    System.out.println("0");
-                }
-                System.out.println();
-            }else {
-                int n = s[i].length() / 8;
-                int m = s[i].length() % 8;
-                if (m == 0) {
-                    for (int k = 0; k) {
-                        System.out.println(s[i].substring(8*k, 8*k + 8));
-                    }
-                }else {
-                    for (int k = 0; k) {
-                        System.out.println(s[i].substring(8*k, 8*k + 8));
-                    }
-                }
-            }
-
-
-        }
-
-        System.out.println();
-
     }
 }

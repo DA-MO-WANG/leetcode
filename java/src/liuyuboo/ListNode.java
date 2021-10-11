611127c4edb77caf7f0e6f5060e8ac3692023e7e
han guo <guohan741@163.com>
2021-10-11 10:00:00 +0800

    autosave leet

--- java/src/liuyuboo/ListNode.java
+++ java/src/liuyuboo/ListNode.java
@@ -1,2 +1,12 @@
-package liuyuboo;public class ListNode {
+package liuyuboo;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode () {}
+    ListNode(int val) {this.val = val;}
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
--- java/src/liuyuboo/S203.java
+++ java/src/liuyuboo/S203.java
@@ -1,2 +1,28 @@
-package liuyuboo;public class S203 {
+package liuyuboo;
+
+public class S203 {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            //delNode(head);
+            ListNode delNode = head;
+            head = head.next;
+            delNode = null;
+        }
+        if (head == null) {
+            return head;
+        }
+
+        while (head.next != null && head.next.val == val) {
+            ListNode delNode = head.next;
+            head.next = head.next.next;
+            delNode = null;
+        }
+        return head;
+    }
+
+    public void delNode(ListNode node) {
+        ListNode temp = node;
+        node = node.next;
+        temp = null;
+    }
 }

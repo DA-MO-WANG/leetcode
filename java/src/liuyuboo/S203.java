32db3367dc1aeda40acbeee04f1414163d42d2c7
han guo <guohan741@163.com>
2021-10-11 11:56:00 +0800

    autosave leet

--- java/src/liuyuboo/S203.java
+++ java/src/liuyuboo/S203.java
@@ -38,7 +38,30 @@ public class S203 {
         return dummy.next;
 
     }
+    //递归来解决
+    public ListNode removeElements3(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        if (head.val == val) {
+            ListNode delNode = head.next;
+            head.next = delNode.next;
+            delNode.next = null;
+        }
+        return removeElements3(head.next,val);
 
+    }
+    public ListNode removeElements4(ListNode node, int val) {
+        if (node == null) {
+            return node;
+        }
+        if (node.next != null && node.next.val == val) {
+            ListNode delNode = node.next;
+            node.next = delNode.next;
+            delNode.next = null;
+        }
+        return removeElements4(node.next,val);
+    }
     public void delNode(ListNode node) {
         ListNode temp = node;
         node = node.next;

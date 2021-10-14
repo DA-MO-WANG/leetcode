6cb68dac7d09f7e75d82c785606648ed1af6a652
han guo <guohan741@163.com>
2021-10-14 18:02:34 +0800

    autosave leet

--- java/src/liuyuboo/LinkedlistProblem.java
+++ java/src/liuyuboo/LinkedlistProblem.java
@@ -1,2 +1,27 @@
-package liuyuboo;public class LinkedlistProblem {
+package liuyuboo;
+
+public class LinkedlistProblem {
+    //s1-迭代版
+    int count(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+    //s1-递归版
+    int count2(ListNode head) {
+        if (head == null) return 0;
+        countdg(head);
+        return size;
+    }
+    int size = 0;
+    void countdg(ListNode head) {
+        size++;
+        if (head == null) {
+            return;
+        }
+        countdg(head.next);
+    }
 }

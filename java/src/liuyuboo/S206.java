8627f28d8db7d6ff0ab7025a03a30c999772752a
han guo <guohan741@163.com>
2021-10-16 18:00:01 +0800

    autosave leet

--- java/src/liuyuboo/S206.java
+++ java/src/liuyuboo/S206.java
@@ -1,5 +1,22 @@
 package liuyuboo;
 
 public class S206 {
-    
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        if (head == null) return null;
+        if (head.next == null) return head;
+        ListNode cur = head;
+        ListNode next = head.next;
+        while (next != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+
+            cur = next;
+            next = next.next;
+
+        }
+        return pre;
+
+    }
 }

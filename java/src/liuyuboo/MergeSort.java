889330ff3b2fd795f640f16f0a0aa0acff15c89e
han guo <guohan741@163.com>
2021-10-23 12:07:00 +0800

    autosave leet

--- java/src/liuyuboo/MergeSort.java
+++ java/src/liuyuboo/MergeSort.java
@@ -69,12 +69,49 @@ public class MergeSort {
         //StringBuffer
         int[] arr = {7,5,6,4};
         MergeSort mergeSort = new MergeSort();
-        mergeSort.mergeSort(arr);
+        int pairs = mergeSort.reversePairs(arr);
         //Arrays.asList(arr).forEach(item->System.out.println(item));
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println("\n---------------");
-        System.out.println(mergeSort.count);
+        System.out.println(pairs);
+    }
+
+    public int reversePairs(int[] nums) {
+        int[] temp = Arrays.copyOf(nums,nums.length);
+        return mergeSort(nums,0,nums.length - 1,temp);
+    }
+    //返回[l,r]区间中的逆序对数量
+    public int mergeSort(int[] arr, int l , int r,int[] temp) {
+        int mid = l + (r - l) / 2;
+        if(l >= r) return 0;
+        int res = 0;
+        res += mergeSort(arr,l,mid,temp);
+        res += mergeSort(arr,mid+1, r, temp);
+        res += merge(arr,l,mid,r,temp);
+        return res;
+    }
+    //合并有序数组[l,mid] [mid+1,r]到一个新的数组,处理l-r区间内的数据
+    public int merge(int[] arr, int l , int mid, int r, int[] temp) {
+        int i = l;
+        int j = mid + 1;
+        int count = 0;
+        for(int k = l; k <= r; k++) {
+            if(i > mid) {
+                arr[k] = temp[j];
+                j++;
+            }else if(j > r) {
+                arr[k] = temp[i];
+                i++;
+            }else if(temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            }else {
+                count += mid - i + 1;
+                j++;
+            }
+        }
+        return count;
     }
 }

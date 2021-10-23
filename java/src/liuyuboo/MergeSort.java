a8496a7321a9dc1544cda2078068e845b66db462
han guo <guohan741@163.com>
2021-10-23 10:39:00 +0800

    autosave leet

--- java/src/liuyuboo/MergeSort.java
+++ java/src/liuyuboo/MergeSort.java
@@ -16,13 +16,14 @@ public class MergeSort {
         mergeSort(arr,mid + 1, r);
         merge(arr,l,mid,r);
     }
+    int count = 0;
     public void merge(int[] arr, int l, int mid, int r) {
         //int[] newarr = Arrays.copyOfRange(arr, l,r + 1);
         int[] newarr = Arrays.copyOf(arr, arr.length);
         int i = l;
         int j = mid + 1;
 
-        /*for (int k = l; k <= r; k++) {
+        for (int k = l; k <= r; k++) {
             //左边数组走完了，只剩下右边数组
             if (i > mid) {
                 arr[k] = newarr[j - l];
@@ -35,13 +36,14 @@ public class MergeSort {
                 i++;
             }else {
                 arr[k] = newarr[j - l];
+                count++;
                 j++;
             }
-        }*/
+        }
 
 
 
-        while (i <= mid && j <= r) {
+       /* while (i <= mid && j <= r) {
             if (newarr[i] < newarr[j]) {
                 arr[l] = newarr[i];
                 i++;
@@ -59,17 +61,18 @@ public class MergeSort {
         while (j <= r) {
             arr[l] = newarr[j];
             j++;
-        }
+        }*/
     }
 
     public static void main(String[] args) {
-        StringBuffer
-        int[] arr = {2,3,1,9,6,4,7};
+        //StringBuffer
+        int[] arr = {7,5,6,4};
         MergeSort mergeSort = new MergeSort();
         mergeSort.mergeSort(arr);
         //Arrays.asList(arr).forEach(item->System.out.println(item));
-        for (int i = 0; i < arr.length; i++) {
+        /*for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
-        }
+        }*/
+        System.out.println(mergeSort.count);
     }
 }

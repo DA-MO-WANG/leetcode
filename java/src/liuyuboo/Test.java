197849255d9d545eb34c15cfda9695dca8cf21ef
han guo <guohan741@163.com>
2021-10-24 12:12:00 +0800

    autosave leet

--- java/src/liuyuboo/Test.java
+++ java/src/liuyuboo/Test.java
@@ -12,16 +12,21 @@ public class Test {
     public static void partQueue(int[] arr) {
         //按分值排队，输出分值也行
         Arrays.sort(arr);
+        int sum = 0;
+        int sumz = 0;
+        int diff = 0;
+
         for (int i = 0; i < arr.length/2; i++) {
+            sumz = arr[i] + arr[arr.length - 1];
             if (i == 0 || i == 3) {
-            System.out.println(arr[i]+" "+ arr[arr.length - 1 - i]);
+                sum = arr[i] + arr[arr.length - 1];
+            System.out.print(arr[i]+" "+ arr[arr.length - 1 - i] + " ");
+            }
         }
-
+        diff = sum * 2 - sumz;
+        System.out.println("本队差值： "+(0 - diff));
     }
-    }
-
-    public static void main(String[] args) {
-        Test test = new Test();
+    public void part() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         String[] s1 = s.split(" ");
@@ -29,10 +34,13 @@ public class Test {
         for (int i = 0; i < s1.length; i++) {
             arr[i] = Integer.parseInt(s1[i]);
         }
-
-
-
         //int[] arr = {3,10,9,5,5,7,8,9};
-        test.partQueue(arr);
+        partQueue(arr);
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        //int[] arr = {3,10,9,5,5,7,8,9};
+        test.part();
     }
 }

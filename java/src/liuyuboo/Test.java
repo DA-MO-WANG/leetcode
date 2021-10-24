6d16994d408d1b9a84e265e150e2adafc926c7f8
han guo <guohan741@163.com>
2021-10-24 13:04:00 +0800

    autosave leet

--- java/src/liuyuboo/Test.java
+++ java/src/liuyuboo/Test.java
@@ -11,17 +11,53 @@ public class Test {
     //保证高手对高手
 
     //最接近的四数之和
-    
-
+    public List<Integer> threeSumClosest(int[] nums, int target) {
+        List<Integer> list = new ArrayList<>();
+        int ret = nums[0] + nums[1] + nums[2];
+        // 先将数组变为升序的
+        Arrays.sort(nums);
+        // 采用双指针遍历
+        for (int i=0; i<nums.length-2; i++) {
+            if (i != 0 && nums[i] == nums[i-1])
+                continue;
+            int m = nums.length-1;
+            for (int j=i+1; j<nums.length-1; j++) {
+                if (j != i+1 && nums[j] == nums[j-1])
+                    continue;
+                // 向左移动指针，且右指针在左指针的右边
+                while(j < m && nums[i]+nums[j]+nums[m]>target) {
+                    // 取最接近的值
+                    if (Math.abs(ret - target) >= Math.abs(nums[i]+nums[j]+nums[m] - target)) {
+                        ret = nums[i]+nums[j]+nums[m];
+                    }
+                    m = m-1;
+                }
+                if (j == m) {
+                    break;
+                }
+                // 取最接近的值
+                if (Math.abs(ret - target) >= Math.abs(nums[i]+nums[j]+nums[m] - target)) {
+                    ret = nums[i]+nums[j]+nums[m];
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[m]);
+                }
+                /*if (ret == target) {
+                    return ret;
+                }*/
+            }
+        }
+        return list;
+    }
     public void partQueue(int[] arr) {
         //按分值排队，输出分值也行
         //Arrays.sort(arr);
+        int[] newarr = new int[7];
         int sumz = getSum(arr);
-        List<List<Integer>> lists = fourSum(arr, sumz << 1);
-
-        for (int i = 0; i < lists.size(); i++) {
-            lists.get(i).stream().forEach(System.out::println);
-            IntSummaryStatistics collect = lists.get(i).stream().collect(Collectors.summarizingInt(value -> value));
+        System.arraycopy(arr,1,newarr,0,newarr.length);
+        List<Integer> list = threeSumClosest(newarr, sumz / 2 - arr[0] );
+            list.stream().forEach(System.out::println);
+            IntSummaryStatistics collect = list.stream().collect(Collectors.summarizingInt(value -> value));
             long sum = collect.getSum();
             System.out.println("本次分队差值： "+ (sum * 2 - sumz));
 

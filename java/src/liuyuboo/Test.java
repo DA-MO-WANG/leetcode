ccef46e444c63b03cdd947ac4c05f0665aeddff5
han guo <guohan741@163.com>
2021-10-24 12:46:00 +0800

    autosave leet

--- java/src/liuyuboo/Test.java
+++ java/src/liuyuboo/Test.java
@@ -10,48 +10,9 @@ public class Test {
     //保证两队分值相差不大
     //保证高手对高手
 
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-        int len = nums.length;
-        //mergeSort(nums, 0, len - 1, new int[len]);
-        Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        if (len < 4) return list;
-        for (int i = 0; i < len - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            for (int j = i + 1; j < len - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                int m = j + 1, n = len - 1;
-                while (m < n) {
-                    int sum = nums[i] + nums[j] + nums[m] + nums[n];
-                    if (sum < target) {
-                        m++;
-                    } else if (sum > target) {
-                        n--;
-                    } else {
-                        List<Integer> l = new ArrayList<>();
-                        l.add(nums[i]);
-                        l.add(nums[j]);
-                        l.add(nums[m]);
-                        l.add(nums[n]);
-                        list.add(l);
-                        m++;
-                        n--;
-                        while (m < n && nums[m] == nums[m - 1]) {
-                            m++;
-                        }
-                        while (m < n && nums[n] == nums[n + 1]) {
-                            n--;
-                        }
-                    }
-                }
-            }
-        }
-        return list;
-    }
+    //最接近的四数之和
+    
+
     public void partQueue(int[] arr) {
         //按分值排队，输出分值也行
         //Arrays.sort(arr);
@@ -84,6 +45,7 @@ public class Test {
         }
         return sum;
     }
+    //处理输入问题，把字符串转化成数组
     public void part() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();

fbf036224adcc4c38a1873dc565c4629ee8fe87a
han guo <guohan741@163.com>
2021-10-25 11:08:00 +0800

    autosave leet

--- java/src/liuyuboo/QuickSort.java
+++ java/src/liuyuboo/QuickSort.java
@@ -1,2 +1,15 @@
-package liuyuboo;public class QuickSort {
+package liuyuboo;
+
+public class QuickSort {
+    private QuickSort() {}
+    private static void sort(int[] arr) {
+
+    }
+    //确定标界点v，让v归位，营造出<v  v  >v 的效果
+    private static int partition(int[] arr, int l, int r) {
+        int v = arr[l];
+        for (int i, j = l; i <= r; i++) {
+            
+        }
+    }
 }

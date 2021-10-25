0cefdb2e47bbbf62d8499fdb83337490452ba91c
han guo <guohan741@163.com>
2021-10-25 11:32:00 +0800

    autosave leet

--- java/src/liuyuboo/QuickSort.java
+++ java/src/liuyuboo/QuickSort.java
@@ -1,16 +1,51 @@
 package liuyuboo;
 
+import java.util.Arrays;
+
+import static com.sun.tools.javac.jvm.ByteCodes.swap;
+
 public class QuickSort {
     private QuickSort() {}
-    private static void sort(int[] arr) {
+    private void sort(int[] arr) {
+        quichSort(arr,0,arr.length - 1);
 
     }
+    public void quichSort(int[] arr, int l , int r) {
+        if (l>= r) return;
+        int p = partition(arr, l, r);
+        quichSort(arr,l,p - 1);
+        quichSort(arr,p + 1, r);
+    }
     //确定标界点v，让v归位，营造出<v  v  >v 的效果
-    private static int partition(int[] arr, int l, int r) {
+    public int partition(int[] arr, int l, int r) {
         int v = arr[l];
-        //i用来
-        for (int i = l,j = l; i <= r; i++) {
-
+        int j = l;
+        //i用来走遍历
+        //j用来作为两部分的分界点，扩容小区间
+        for (int i = l + 1; i <= r; i++) {
+            //暂时不处理v有重复的情况
+            if (arr[i] > v) {
+                //大区间：j+1,i
+                break;
+            }else {
+                //j++为交换做准备，通过交换扩容小区间
+                j++;
+                swap(arr,i,j);
+            }
         }
+        swap(arr,l,j);
+        return j;
+    }
+    public void swap(int[] arr, int i , int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,9,5,3,2,4};
+        QuickSort q = new QuickSort();
+        q.sort(arr);
+        Arrays.asList(arr).stream().forEach(System.out::println);
     }
 }

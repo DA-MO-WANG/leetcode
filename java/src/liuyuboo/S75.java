dbd02e1217e7d2d27bd548caa6fab609829c96ef
han guo <guohan741@163.com>
2021-10-29 11:58:01 +0800

    autosave leet

--- java/src/liuyuboo/S75.java
+++ java/src/liuyuboo/S75.java
@@ -6,27 +6,30 @@ public class S75 {
         if (nums.length == 1) {
             return;
         }
-        if (nums.length)
+        //if (nums.length)
 
         //分成三部分
+        //boolean flag = false;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
                 swap(nums,i,0);
+                //flag = true;
             }
         }
+        int v = nums[0];
         int lt = 0;
         int gt = nums.length;
         int i = 1;
         while (i < nums.length && i < gt) {
-            if (nums[i] == 1) {
+            if (nums[i] == v) {
                 i++;
             }
-            if (nums[i] < 1) {
+            if (nums[i] < v) {
                 swap(nums,lt + 1,i);
                 lt++;
                 i++;
             }
-            if (nums[i] > 1) {
+            if (nums[i] > v) {
                 swap(nums,gt - 1,i);
                 gt--;
             }
@@ -40,7 +43,7 @@ public class S75 {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{2,2,1,0,1,0,2,1,0};
+        int[] arr = new int[]{2,2,0,2,0};
         S75 s75 = new S75();
         s75.sortColors(arr);
         for (int i : arr) {

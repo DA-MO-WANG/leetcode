9111c30c096afb73c5f4bceaae3e74db70a662c5
han guo <guohan741@163.com>
2021-10-29 15:55:00 +0800

    autosave leet

--- java/src/liuyuboo/S215.java
+++ java/src/liuyuboo/S215.java
@@ -30,7 +30,7 @@ public class S215 {
     public int partition(int[] arr, int l ,int r, Random random) {
         int p = l + random.nextInt(r - l + 1);
         swap(arr,l,p);
-        int v = arr[p];
+        int v = arr[l];
         int lt = l;
         int gt = r;
         int i = l + 1;

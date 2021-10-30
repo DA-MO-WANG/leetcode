3af9d2cb065285e6487773b7c84701c3d608dbe7
han guo <guohan741@163.com>
2021-10-30 11:49:00 +0800

    autosave leet

--- java/src/liuyuboo/BinarySearch.java
+++ java/src/liuyuboo/BinarySearch.java
@@ -1,2 +1,13 @@
-package liuyuboo;public class BinarySearch {
+package liuyuboo;
+
+public class BinarySearch {
+    public <E extends Comparable<E>> int search(E[] data, E target) {
+        int lo = 0;
+        int hi = data.length - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) >> 1;
+            if (target.compareTo(data[mid]))
+        }
+    }
 }

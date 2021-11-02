66b9b248455bd40f5601ed757dbf2f9796542c6f
han guo <guohan741@163.com>
2021-11-02 11:14:00 +0800

    autosave leet

--- java/src/liuyuboo/BinarySearch.java
+++ java/src/liuyuboo/BinarySearch.java
@@ -77,7 +77,7 @@ public class BinarySearch {
         int search = b.<Character>search(letter, le);
         System.out.println(search == str.indexOf('f'));*/
         BinarySearch b = new BinarySearch();
-        int lower_ceil = b.lower_ceil(new int[]{2, 4, 7, 1, 6, 3, 11}, 1);
+        int lower_ceil = b.lower_ceil(new int[]{1,2, 4, 7, 11}, 1);
         System.out.println(lower_ceil);
         //System.out.println(-1/2);
 

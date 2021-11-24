632c529efd23b4a92944ec42ec7e4115db289993
han guo <guohan741@163.com>
2021-11-24 17:37:00 +0800

    autosave leet

--- java/src/liuyuboo/huisu.java
+++ java/src/liuyuboo/huisu.java
@@ -1,2 +1,16 @@
-package liuyuboo;public class huisu {
+package liuyuboo;
+
+public class huisu {
+    boolean hasPath(char[][] matrix, int rows, int cols, String str) {
+        if (matrix == null || rows < 1 || cols < 1 || str == null) {
+            return false;
+        }
+        boolean[][] visited = new boolean[rows][cols];
+        int pathLength = 0;
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (hasPathCore)
+            }
+        }
+    }
 }

5165ef13b396b38de9a037bc807d8a3fb7e5a358
han guo <guohan741@163.com>
2021-11-24 18:15:00 +0800

    autosave leet

--- java/src/liuyuboo/huisu.java
+++ java/src/liuyuboo/huisu.java
@@ -1,7 +1,7 @@
 package liuyuboo;
 
 public class huisu {
-    boolean hasPath(char[][] matrix, int rows, int cols, String str) {
+    boolean hasPath(char[][] matrix, int rows, int cols, char[] str) {
         if (matrix == null || rows < 1 || cols < 1 || str == null) {
             return false;
         }
@@ -9,9 +9,13 @@ public class huisu {
         int pathLength = 0;
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < cols; ++col) {
-                if (hasPathCore)
+                if (hasPathCore(matrix,rows,cols,row,col,str,pathLength,visited)) {
+                    return true;
+                }
             }
         }
+        visited = null;
+        return false;
     }
 
     boolean hasPathCore(char[][] matrix, int rows,int cols,int row,int col,char[] str,int pathLength, boolean[][] visited) {
@@ -20,7 +24,17 @@ public class huisu {
         }
         boolean hasPath = false;
         if (row >= 0 && row < rows && col >= 0 && col < cols && matrix[row][col]== str[pathLength] && !visited[row][col]) {
-            
+            ++pathLength;
+            visited[row][col] = true;
+            hasPath = hasPathCore(matrix,rows,cols,row,col - 1,str,pathLength,visited)
+                    || hasPathCore(matrix,rows,cols,row - 1,col,str,pathLength,visited)
+                    || hasPathCore(matrix,rows,cols,row,col + 1,str,pathLength,visited)
+                    || hasPathCore(matrix,rows,cols,row + 1,col,str,pathLength,visited);
+            if (!hasPath) {
+                --pathLength;
+                visited[row][col] = false;
+            }
         }
+        return hasPath;
     }
 }

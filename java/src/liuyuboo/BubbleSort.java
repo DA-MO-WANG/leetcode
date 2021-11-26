e3135324945de576302e823962332558239d0e52
han guo <guohan741@163.com>
2021-11-26 12:03:00 +0800

    autosave leet

--- java/src/liuyuboo/BubbleSort.java
+++ java/src/liuyuboo/BubbleSort.java
@@ -1,12 +1,19 @@
 package liuyuboo;
 
 public class BubbleSort<E extends Comparable<E>> {
+    //优化看数据情况
     public void bubbleSort(E[] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length - i - 1; j++) {
+        boolean flag = false;
+        for (int i = 0; i < data.length; i++) {//第一个循环是轮数
+
+            for (int j = 0; j < data.length - i - 1; j++) {//第二个循环是比较环节
                 if (data[j].compareTo(data[j+1]) > 0) {
+                    flag = true;
                     swap(data,j,j+1);
                 }
+            }
+            if (flag) {
+                break;
             }
         }
 
@@ -19,7 +26,7 @@ public class BubbleSort<E extends Comparable<E>> {
 
     public static void main(String[] args) {
         BubbleSort bubbleSort = new BubbleSort();
-        Integer[] arr = new Integer[]{3,1,4,2,7};
+        Integer[] arr = new Integer[]{1,2,3,4};
         bubbleSort.bubbleSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");

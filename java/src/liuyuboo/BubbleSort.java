4a19ac68eedf44e381128158a2671b25b31cd495
han guo <guohan741@163.com>
2021-11-26 11:58:00 +0800

    autosave leet

--- java/src/liuyuboo/BubbleSort.java
+++ java/src/liuyuboo/BubbleSort.java
@@ -1,2 +1,29 @@
-package liuyuboo;public class BubbleSort {
+package liuyuboo;
+
+public class BubbleSort<E extends Comparable<E>> {
+    public void bubbleSort(E[] data) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j].compareTo(data[j+1]) > 0) {
+                    swap(data,j,j+1);
+                }
+            }
+        }
+
+    }
+    public void swap(E[] data, int i, int j) {
+        E temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        Integer[] arr = new Integer[]{3,1,4,2,7};
+        bubbleSort.bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
 }

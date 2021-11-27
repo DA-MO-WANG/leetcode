868b521c9aeea0fbfa34e0a8ea230a030b069827
han guo <guohan741@163.com>
2021-11-27 13:15:00 +0800

    autosave leet

--- java/src/liuyuboo/S162.java
+++ java/src/liuyuboo/S162.java
@@ -2,15 +2,18 @@ package liuyuboo;
 
 public class S162 {
     public int findPeakElement(int[] nums) {
+        //这个题必然有峰值，就看在哪
         int i = -1;
         int j = nums.length;
         while (j > i) {
             int mid = i + (j - i) / 2;
+            //核心检测条件
             if ( (mid == 0 && nums[mid] > nums[mid + 1]) || (mid == nums.length - 1 && nums[mid] > nums[mid - 1]) || (mid > 0 && nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])) {
                 return mid;
             }
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                j = mid;
+            //移动分支：推出哪一端必然有
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {//（-1，mid)处必有峰值，单调有，不单调也有
+                j = mid;//往哪个方向走，就从另外一个边出发推
             }else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) {
                 i = mid;
             }

fbe05f74092def253eede66aa6854254f769cbf8
han guo <guohan741@163.com>
2021-11-30 13:18:00 +0800

    autosave leet

--- java/src/liuyuboo/J6.java
+++ java/src/liuyuboo/J6.java
@@ -1,7 +1,18 @@
 package liuyuboo;
 
 public class J6 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        //先拿到第一层根节点:去前序序列
+        int val = preorder[0];
+        //找到中序序列的左右子序列：把中序序列分成左右两部分
 
     }
+
+    public int getIndex(int[])
 }

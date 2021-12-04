8e18d7af0d65cf595f02d9a23e0e5858f80d9956
han guo <guohan741@163.com>
2021-12-04 23:00:00 +0800

    autosave leet

--- java/src/liuyuboo/J10.java
+++ java/src/liuyuboo/J10.java
@@ -1,5 +1,22 @@
 package liuyuboo;
 
 public class J10 {
-    public 
+    public int hammingWeight(int n) {
+        int m = 1;
+        int count = 0;
+        while (n >= m) {
+            if ((n & m) == m) {
+                count++;
+            }
+            m = m << 1;
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        J10 j10 = new J10();
+        int i = j10.hammingWeight(9);
+        System.out.println(i);
+    }
 }

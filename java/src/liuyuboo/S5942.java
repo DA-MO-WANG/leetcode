866b51ff9f40f08b956850f233d3261e267d2b04
han guo <guohan741@163.com>
2021-12-05 14:06:00 +0800

    autosave leet

--- java/src/liuyuboo/S5942.java
+++ java/src/liuyuboo/S5942.java
@@ -115,7 +115,7 @@ public class S5942 {
                 return;
             }
             for(int i=count;i< arr.length;i++){
-                if(i>count && arr[i]==arr[i-1]) continue;
+                //if(i>count && arr[i]==arr[i-1]) continue;
                 list.add(arr[i]);
                 backtrack(arr,k,i+1);
                 list.remove(list.size()-1);

a53b7222e5998183b4d4dcc9cccf500e2bcd4b98
han guo <guohan741@163.com>
2021-12-05 11:25:00 +0800

    autosave leet

--- java/src/liuyuboo/S5942.java
+++ java/src/liuyuboo/S5942.java
@@ -1,2 +1,38 @@
-package liuyuboo;public class S5942 {
+package liuyuboo;
+
+import java.util.Arrays;
+
+public class S5942 {
+    public int[] getOrder(int[] nums) {
+        Arrays.sort(nums);
+        Set<Integer> set = new BSTSet<>();
+
+
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] == 0) {
+                        continue;
+                    }
+
+                        int three = nums[i] * 100;
+                        for (int j = 0; j < nums.length; j++) {
+                            if (j == i) {
+                                continue;
+                            }
+                            int two = nums[i] * 10;
+                            for (int q = 0; q < nums.length; q++) {
+                                if (q == i || q == j || nums[q] % 2 != 0) {
+                                    continue;
+                                }
+                                int one = nums[i] * 1;
+                                set.add(three + two + one);
+                            }
+
+                }
+            }
+            for ()
+        }
+
+    }
+
+
 }

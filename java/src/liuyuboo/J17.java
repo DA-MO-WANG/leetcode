90410ddfc4100c87461da3f54b7a12e8cf97a79e
han guo <guohan741@163.com>
2021-12-08 20:55:00 +0800

    autosave leet

--- java/src/liuyuboo/J17.java
+++ java/src/liuyuboo/J17.java
@@ -5,19 +5,24 @@ public class J17 {
         //难点1:大数的问题越界
         int[] wei = new int[n];
 
-        for (int i = wei.length - 1; i < wei.length;) {
-            wei[i] = wei[i] + 1;
-            if(wei[i] == 10) {
-                wei[i + 1] = wei[i + 1] + 1;
-                wei[i] = 0;
-                i = i + 1;
-            }else if (wei[i + 1]){
 
-            }
+
         }
 
 
         //难点2:打印问题
 
     }
+
+    public void add(int[] wei,int n,int m) {
+        if (m > n) {
+            return;
+        }
+        for (int i = 1; i <= m; ) {
+            wei[n - i]++;
+            if (wei[n - i] == 10) {
+                wei[n - i + 1]++;
+                wei[n - i] = 0;
+            }
+        }
 }

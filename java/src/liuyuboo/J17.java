6b7c98b2e98a9dc0e01a3e9808c336fccb69dd5f
han guo <guohan741@163.com>
2021-12-08 20:43:00 +0800

    autosave leet

--- java/src/liuyuboo/J17.java
+++ java/src/liuyuboo/J17.java
@@ -1,2 +1,23 @@
-package liuyuboo;public class J17 {
+package liuyuboo;
+
+public class J17 {
+    public int[] printNumbers(int n) {
+        //难点1:大数的问题越界
+        int[] wei = new int[n];
+
+        for (int i = wei.length - 1; i < wei.length;) {
+            wei[i] = wei[i] + 1;
+            if(wei[i] == 10) {
+                wei[i + 1] = wei[i + 1] + 1;
+                wei[i] = 0;
+                i = i + 1;
+            }else if (wei[i + 1]){
+
+            }
+        }
+
+
+        //难点2:打印问题
+
+    }
 }

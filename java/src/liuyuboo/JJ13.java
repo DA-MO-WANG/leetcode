da6ab9a49032b6c439c47779ba43e17c1ac3b9ca
han guo <guohan741@163.com>
2021-12-09 21:04:00 +0800

    autosave leet

--- java/src/liuyuboo/JJ13.java
+++ java/src/liuyuboo/JJ13.java
@@ -1,2 +1,26 @@
-package liuyuboo;public class JJ13 {
+package liuyuboo;
+
+public class JJ13 {
+
+    //O1复杂度
+    //所谓思路的全面性---你写的代码，自己查找成立的前提
+    void deleteNode(ListNode head, ListNode deleted) {
+        if (deleted.next != null) {
+            ListNode dnode = deleted.next;
+            deleted.val = dnode.val;//dnode 不为null
+            deleted.next = dnode.next;
+        }else {
+            ListNode cur = head;
+            if(cur.next != null) {
+                while (cur.next.next != null) {//cur.next != null
+                    cur = cur.next;
+                }
+                cur.next = null;
+            }else {
+                head = null;
+            }
+
+
+    }
+
 }

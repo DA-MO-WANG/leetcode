e05aa535edfd001b0039ae4de1dfd53f629cb85d
han guo <guohan741@163.com>
2021-12-09 17:46:00 +0800

    autosave leet

--- java/src/liuyuboo/J17.java
+++ java/src/liuyuboo/J17.java
@@ -18,28 +18,25 @@ public class J17 {
     //第二次照抄
     //n位数字可能会突破任何正数类型的范围--只能用字符串来表示
     public void printToMaxOfNDigits(int n) {
-
-
-        //ArrayList<ArrayList> lists = new ArrayList<>();
-        //难点1:大数的问题越界
-        String str = "";
-        //ArrayList<String> list = null;
-        ArrayList<String> list2 = null;
+        //所有的数字都只是0-9这10个数表示
+        //找到n个位置，模拟这10个数字的变化
         if (n <= 0) {
             throw new IllegalArgumentException("不合法的参数！");
         }
-        char[] number = new char[n];
+        //所以说我需要一个数组来表示这n个位置
+        char[] number = new char[n + 1];
+        //关卡1：什么时候超过n位最大数---此时要停止模拟
+        //因为要腾出一个最高位置，作为预警---数组是n+1==>第一位置从0变成1时，此时触发了
         while (!increment(number)) {
-            str = printNumbers(number);
-            list2.add(str);
-            //lists.add(list);
+            printNumbers(number);
+
         }
-
-
     }
 
     public boolean increment(char[] number) {
+        //设计一个标识位来标志溢出标志--因为只有两种状态--布尔型
         boolean isOverFlow = false;
+        
         int nTakeOver = 0;
         int nLength = number.length;
         for (int i = nLength - 1; i >= 0; i--) {

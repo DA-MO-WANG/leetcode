f8545180f3b4a96a1945a10cb5a53805a5a3dfe7
han guo <guohan741@163.com>
2021-12-10 15:47:00 +0800

    autosave leet

--- java/src/liuyuboo/J21.java
+++ java/src/liuyuboo/J21.java
@@ -1,5 +1,27 @@
 package liuyuboo;
 
 public class J21 {
-    
+    public int[] exchange(int[] nums) {
+        exchange1(nums);
+        return nums;
+    }
+    public void exchange1(int[] nums) {
+        int l = 0;
+        int gt = nums.length - 1;
+        for (int i = l; i < gt;) {
+            if ((nums[i] & 1) == 1) {
+                i++;
+            }else {
+                swap(nums,i,gt);
+                gt--;
+            }
+        }
+    }
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 }

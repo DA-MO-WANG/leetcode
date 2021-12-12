bb5613426e9aee1802f047343bec5ad807441406
han guo <guohan741@163.com>
2021-12-12 13:56:00 +0800

    autosave leet

--- java/src/liuyuboo/Z5954.java
+++ java/src/liuyuboo/Z5954.java
@@ -89,9 +89,9 @@ public class Z5954 {
 
     public static void main(String[] args) {
         Z5954 z = new Z5954();
-        int[] arr = {726,739,934,116,643,648,473,984,482,85,850,806,146,764,156,66,186,339,985,237,662,552,800,78,617,933,481,652,796,594,151,82,183,241,525,221,951,732,799,483,368,354,776,175,974,187,913,842};
-        int alice = 1439;
-        int bob = 1207;
+        int[] arr = {1,2,4,4,5};
+        int alice = 6;
+        int bob = 5;
         int i = z.minimumRefill(arr, alice, bob);
         System.out.println(i);
 

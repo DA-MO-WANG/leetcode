c3476a3a8b6a63ff468cde38c49775089f45a3df
han guo <guohan741@163.com>
2021-12-12 17:57:01 +0800

    autosave leet

--- java/src/liuyuboo/J26.java
+++ java/src/liuyuboo/J26.java
@@ -2,6 +2,20 @@ package liuyuboo;
 
 public class J26 {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return isSubStructure1(A,B);
 
+
+    }
+    public boolean isSubStructure1(TreeNode max, TreeNode min) {
+        if (max == null || min == null) {
+            return true;
+        }
+        if (max.val == min.val) {
+            return isSubStructure1(max.left,min.left) && isSubStructure1(max.right,min.right);
+        }else if (max.val > min.val) {
+            return isSubStructure1(max.left,min);
+        }else {
+            return isSubStructure(max.right,min);
+        }
     }
 }
--- java/src/liuyuboo/TreeNode.java
+++ java/src/liuyuboo/TreeNode.java
@@ -1,2 +1,8 @@
-package liuyuboo;public class TreeNode {
+package liuyuboo;
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
 }

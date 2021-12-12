06ab26c950e0af535482062b53131e3c842ce8df
han guo <guohan741@163.com>
2021-12-12 12:52:00 +0800

    autosave leet

--- java/src/liuyuboo/Z5954.java
+++ java/src/liuyuboo/Z5954.java
@@ -1,2 +1,41 @@
-package liuyuboo;public class Z5954 {
+package liuyuboo;
+
+public class Z5954 {
+    public int minimumRefill(int[] plants, int capacityA, int capacityB) {
+        int[] dp = new int[plants.length];
+        int alice = capacityA;
+        int bob = capacityB;
+        int left = 0;
+        int right = plants.length - 1;
+        while (left < right) {
+            alice = water(plants,dp,capacityA,left,alice);
+            bob = water(plants,dp,capacityB,right,bob);
+            left++;
+            right--;
+        }
+        int sum = 0;
+        for (int i = 0; i < dp.length; i++) {
+            sum += dp[i];
+        }
+        return sum;
+    }
+
+    public int water(int[] plants, int[] dp,int capacity, int index, int role) {
+        int diff = role - plants[index];
+        if(diff < 0) {
+            int c = Math.abs(diff) % capacity == 0 ? diff / capacity : 1 + diff / capacity;
+            dp[index] = c;
+            role = c * capacity + diff;
+        }else {
+            dp[index] = 0;
+            role = diff;
+        }
+        return role;
+    }
+
+    public static void main(String[] args) {
+        Z5954 z = new Z5954();
+        int[] arr = {};
+        z.minimumRefill()
+    }
 }

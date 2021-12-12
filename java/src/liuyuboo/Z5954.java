e9238b25d3fe606d36a859b5a4429f618051dc87
han guo <guohan741@163.com>
2021-12-12 14:13:00 +0800

    autosave leet

--- java/src/liuyuboo/Z5954.java
+++ java/src/liuyuboo/Z5954.java
@@ -59,9 +59,7 @@ public class Z5954 {
                 }
                 break;
             }
-            if(left == right - 1) {
-                System.out.println(1111);
-            }
+
             alice = water(plants,dp,capacityA,left,alice);
             bob = water(plants,dp,capacityB,right,bob);
             left++;
@@ -77,9 +75,10 @@ public class Z5954 {
     public int water(int[] plants, int[] dp,int capacity, int index, int role) {
         int diff = role - plants[index];
         if(diff < 0) {
-            int c = Math.abs(diff) % capacity == 0 ? Math.abs(diff) / capacity : 1 + Math.abs(diff) / capacity;
+            //int c = Math.abs(diff) % capacity == 0 ? Math.abs(diff) / capacity : 1 + Math.abs(diff) / capacity;
+            int c = plants[index] % capacity == 0 ? plants[index] / capacity : plants[index] / capacity + 1;
             dp[index] = c;
-            role = c * capacity + diff;
+            role = c * capacity - plants[index];
         }else {
             dp[index] = 0;
             role = diff;
@@ -92,7 +91,7 @@ public class Z5954 {
         int[] arr = {726,739,934,116,643,648,473,984,482,85,850,806,146,764,156,66,186,339,985,237,662,552,800,78,617,933,481,652,796,594,151,82,183,241,525,221,951,732,799,483,368,354,776,175,974,187,913,842};
         int alice = 1439;
         int bob = 1207;
-        int i = z.minimumRefill(arr, alice, bob);
+        int i = z.minimumRefill1(arr, alice, bob);
         System.out.println(i);
 
     }

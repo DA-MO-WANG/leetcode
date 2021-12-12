52325e28de5acebad7289d64cb969349d116f0ce
han guo <guohan741@163.com>
2021-12-12 11:38:00 +0800

    autosave leet

--- java/src/liuyuboo/Z5952.java
+++ java/src/liuyuboo/Z5952.java
@@ -8,24 +8,24 @@ import java.util.Set;
 public class Z5952 {
     public int countPoints(String rings) {
         int size = rings.length();
-        HashMap<Character,HashSet<Character>> map = new HashMap<>();
+        HashMap<String,HashSet<String>> map = new HashMap<>();
         int count = 0;
         int sum = 0;
 
 
         for(int i = 1; i < size; i = i + 2) {
-            Character gan = rings.charAt(i);
-            Character huan = rings.charAt(i - 1);
+            String gan = rings.charAt(i) + "";
+            String huan = rings.charAt(i - 1) + "";
             if (map.containsKey(gan)) {
                 map.get(gan).add(huan);
             }
-            HashSet<Character> set = new HashSet<>();
+            HashSet<String> set = new HashSet<>();
             set.add(huan);
             map.put(gan,set);
         }
         int ret = 0;
-        Set<Map.Entry<Character, HashSet<Character>>> entries = map.entrySet();
-        for (Map.Entry<Character, HashSet<Character>> entry : entries) {
+        Set<Map.Entry<String, HashSet<String>>> entries = map.entrySet();
+        for (Map.Entry<String, HashSet<String>> entry : entries) {
             if(entry.getValue().size() >= 3) {
                 ret++;
             }

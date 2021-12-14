def6634c9a90cc37dfed9f992f9cb4c3eb149979
han guo <guohan741@163.com>
2021-12-14 22:57:00 +0800

    autosave leet

--- java/src/liuyuboo/S630.java
+++ java/src/liuyuboo/S630.java
@@ -1,2 +1,21 @@
-package liuyuboo;public class S630 {
+package liuyuboo;
+
+public class S630 {
+    public int scheduleCourse(int[][] courses) {
+        int rows = courses.length;
+        int cols = courses[0].length;
+        boolean[] hg = new boolean[rows];
+        for (int i = 0; i < rows; i++) {
+
+            if (courses[i][0] > courses[i][1]) {
+                hg[i] = true;
+            }
+
+        }
+
+        
+
+
+    }
+    public
 }

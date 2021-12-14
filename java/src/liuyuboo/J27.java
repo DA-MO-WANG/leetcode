c70f31a11fd9fc348e2cb9781a750ef1da2154e7
han guo <guohan741@163.com>
2021-12-14 12:35:00 +0800

    autosave leet

--- java/src/liuyuboo/J27.java
+++ java/src/liuyuboo/J27.java
@@ -1,4 +1,16 @@
 package liuyuboo;
 
 public class J27 {
+    //返回镜像后的树的根节点
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        TreeNode newright = mirrorTree(root.left);
+        TreeNode newleft = mirrorTree(root.right);
+        root.left = newleft;
+        root.right = newright;
+        return root;
+
+    }
 }

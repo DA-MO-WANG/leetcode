39ade830f826aa1789dae5691ac08f889cd05588
han guo <guohan741@163.com>
2021-12-15 23:31:00 +0800

    autosave leet

--- java/src/liuyuboo/S1626.java
+++ java/src/liuyuboo/S1626.java
@@ -1,2 +1,11 @@
-package liuyuboo;public class S1626 {
+package liuyuboo;
+
+public class S1626 {
+    public int calculate(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ()
+
+        }
+    }
 }

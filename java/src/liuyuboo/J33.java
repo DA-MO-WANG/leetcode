a3b192cdd38dd0df30934a5f2e232e04504d95ad
han guo <guohan741@163.com>
2021-12-18 21:42:00 +0800

    autosave leet

--- java/src/liuyuboo/J33.java
+++ java/src/liuyuboo/J33.java
@@ -1,5 +1,36 @@
 package liuyuboo;
 
 public class J33 {
-    
+    public boolean verifyPostorder(int[] postorder) {
+       return verifyPostorder(postorder,0,postorder.length - 1);
+    }
+
+    public boolean verifyPostorder(int[] postorder, int left1, int right2) {
+        int root = postorder[right2];
+        int i = left1;
+        for (; i < right2; i++) {
+            if (postorder[i] > root) {
+                break;
+            }
+        }
+        int j = i;
+        for (;j < right2; j++) {
+            if (postorder[j] < root) {
+                return false;
+            }
+        }
+
+        //
+        boolean left = true;
+        if (i > 0) {
+            left = verifyPostorder(postorder,left1,i - 1);
+        }
+        boolean right = true;
+        for (;i < postorder.length - 1; i++) {
+            right = verifyPostorder(postorder,i,right2 - 1);
+        }
+        return left && right;
+    }
+
+
 }

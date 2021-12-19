21485d03d50753fbffec87f9721edbd9ed511fed
han guo <guohan741@163.com>
2021-12-19 10:58:00 +0800

    autosave leet

--- java/src/liuyuboo/Z272.java
+++ java/src/liuyuboo/Z272.java
@@ -1,2 +1,16 @@
-package liuyuboo;public class Z272 {
+package liuyuboo;
+
+public class Z272 {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuffer sbz = new StringBuffer();
+        for(int i = 1; i < spaces.length; i++) {
+
+            StringBuffer sb = new StringBuffer(s.substring(spaces[i - 1],spaces[i] - 1));
+            sb.append(" ");
+            sbz.append(sb);
+
+
+        }
+        return sbz.toString();
+    }
 }

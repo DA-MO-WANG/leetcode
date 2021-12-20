516389923fca4b5a29b1293ed7a5a569d6e5b431
han guo <guohan741@163.com>
2021-12-20 14:57:00 +0800

    autosave leet

--- java/src/liuyuboo/J34.java
+++ java/src/liuyuboo/J34.java
@@ -1,6 +1,6 @@
 package liuyuboo;
 
-import edu.princeton.cs.algs4.In;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,15 +9,22 @@ public class J34 {
 
     public List<List<Integer>> pathSum(TreeNode root, int target) {
         if(root == null) return lists;
-        ArrayList<Integer> path = new ArrayList<>();
+        Stack<Integer> path = new ArrayList();
         int currentSum = 0;
         findPath(root,target,path,currentSum);
 
     }
     ArrayList<Integer> list = new ArrayList<>();
     List<List<Integer>> lists = new ArrayList<>();
-    public void findPath(TreeNode root, int target, ArrayList<Integer> path, int currentSum ) {
+    public void findPath(TreeNode root, int target, Stack<Integer> path, int currentSum ) {
         currentSum += root.val;
+        path.push(root.val);
+        boolean isLeaf = root.left == null && root.right == null;
+        if (currentSum == target && isLeaf) {
+            for (Integer v : path) {
+
+            }
+        }
 
         pathSum(root.left,target - root.val,sum + root.val);
         pathSum(root.right,target - root.val,sum + root.val);

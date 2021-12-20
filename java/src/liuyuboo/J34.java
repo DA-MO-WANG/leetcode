2665d9acf028bc223894ed00981e7de0106b8500
han guo <guohan741@163.com>
2021-12-20 15:14:00 +0800

    autosave leet

--- java/src/liuyuboo/J34.java
+++ java/src/liuyuboo/J34.java
@@ -3,31 +3,59 @@ package liuyuboo;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class J34 {
+    public class Stack<E> {
+        LinkedList<E> list;
+        public Stack(){
+            list = new LinkedList();
+        }
+        public E pop(){
+            return list.removeLast();
+        }
+        public void push(E o){
+            list.add(o);
+        }
+        public E getTop(){
+            return list.getLast();
+        }
+        public boolean isEmpty(){
+            return list.size()==0;
+        }
+        public int size(){
+            return list.size();
+        }
 
     public List<List<Integer>> pathSum(TreeNode root, int target) {
         if(root == null) return lists;
-        Stack<Integer> path = new ArrayList();
+        Stack<Integer> path = new Stack<>();
         int currentSum = 0;
         findPath(root,target,path,currentSum);
+        return lists;
 
     }
-    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> pathlist = new ArrayList<>();
     List<List<Integer>> lists = new ArrayList<>();
     public void findPath(TreeNode root, int target, Stack<Integer> path, int currentSum ) {
         currentSum += root.val;
         path.push(root.val);
         boolean isLeaf = root.left == null && root.right == null;
         if (currentSum == target && isLeaf) {
-            for (Integer v : path) {
-
+            while (!path.isEmpty()) {
+                pathlist.add(path.pop());
             }
+            lists.add(pathlist);
+            pathlist.clear();
         }
-
-        pathSum(root.left,target - root.val,sum + root.val);
-        pathSum(root.right,target - root.val,sum + root.val);
+        if (root.left != null) {
+            findPath(root.left,target,path,currentSum);
+        }
+        if (root.right != null) {
+            findPath(root.right,target,path,currentSum);
+        }
+        path.pop();
     }
 
 

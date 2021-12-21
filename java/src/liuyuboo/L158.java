cb6a319847e76920416dce932f66cf2f525c4ea3
han guo <guohan741@163.com>
2021-12-21 12:35:00 +0800

    autosave leet

--- java/src/liuyuboo/L158.java
+++ java/src/liuyuboo/L158.java
@@ -2,7 +2,47 @@ package liuyuboo;
 
 public class L158 {
     class Node{
-        
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
     }
-    public Node copyRandomList(Node head)
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Node cur = head;
+        while (cur != null) {
+            Node copyNode = new Node(cur.val);
+            copyNode.next = cur.next;
+            cur.next = copyNode;
+            cur = cur.next.next;
+        }
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+        }
+        Node copyHead = head.next;
+        cur = head;
+        Node curCopy = head.next;
+        while (cur != null) {
+            cur.next = cur.next.next;
+            cur = cur.next;
+            if (curCopy.next != null) {
+                curCopy.next = curCopy.next.next;
+                curCopy = curCopy.next;
+            }
+        }
+        return copyHead;
+
+
+
+    }
 }

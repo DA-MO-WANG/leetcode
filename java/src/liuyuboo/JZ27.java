2721155e9dc88bb60a569bf10abae6a06dd67f6c
han guo <guohan741@163.com>
2021-12-27 10:24:00 +0800

    autosave leet

--- java/src/liuyuboo/JZ27.java
+++ java/src/liuyuboo/JZ27.java
@@ -1,5 +1,25 @@
 package liuyuboo;
 
 public class JZ27 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+
+    }
+
     
 }

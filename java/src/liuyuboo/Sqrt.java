dfa2b0e80c1cd0c608da5a19cb499303f6c1dcaa
han guo <guohan741@163.com>
2022-01-02 18:16:01 +0800

    autosave leet

--- java/src/liuyuboo/Sqrt.java
+++ java/src/liuyuboo/Sqrt.java
@@ -1,5 +1,19 @@
 package liuyuboo;
 
 public class Sqrt {
-    
+    int[] data;
+    int[] blocks;
+    int B;//存放组数
+    int bn;//存放每块数量
+    public Sqrt(int[] nums) {
+        data = nums;
+        B = nums.length / 1;
+        for () {
+            
+        }
+    }
+
+    public int sumRange(int left, int right) {
+
+    }
 }

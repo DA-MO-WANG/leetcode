0981197a92a3e25f1d525826b9676bfb88835e09
han guo <guohan741@163.com>
2022-01-21 09:05:00 +0800

    autosave leet

--- java/src/liuyuboo/S187.java
+++ java/src/liuyuboo/S187.java
@@ -1,2 +1,23 @@
-package liuyuboo;public class S187 {
+package liuyuboo;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class S187 {
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> res = new HashSet<>();
+        for (int i = 0; i + 10 < s.length(); i++) {
+            String key = s.substring(i,i + 10);
+            if (seen.contains(key)) {
+                res.add(key);
+            }else {
+                seen.add(key);
+            }
+        }
+        return new ArrayList<>(res);
+
+    }
 }

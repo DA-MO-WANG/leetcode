ecc900079b192572bcb047b38648883b90a0a8b7
han guo <guohan741@163.com>
2022-02-14 15:40:00 +0800

    autosave leet

--- go/echo1.go
+++ go/echo1.go
@@ -3,10 +3,11 @@ import "fmt"
 import "os"
 
 func main() {
-    var s, sep string   
+    var s, sep string
+    //关键就在os.Args---对它的认知就是数组
+    //   
     for i := 1; i < len(os.Args); i++ {
-	s += sep + os.Args[i]
-	sep = " "
+	s +=  os.Args[i]
     }
     fmt.Println(s)
 }

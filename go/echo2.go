420afa28feedec705eb3f5e2b7d1f715b28621fb
han guo <guohan741@163.com>
2022-02-15 16:51:00 +0800

    autosave leet

--- go/echo2.go
+++ go/echo2.go
@@ -1,12 +1,13 @@
 package main
 import "fmt"
 import "os"
+import "strconv"
 
 func main() {
    s, sep := "", ""
    for index,arg := range os.Args[1:] {
       
-      s += sep + arg + "-" + strconv.Itoa(x)
+      s += sep + arg + "-" + strconv.Itoa(index)
       sep = " "
    }
    fmt.Println(s)

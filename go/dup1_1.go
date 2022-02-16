d68b0f06b3e333047a04eea67dd1445fb5e1a573
han guo <guohan741@163.com>
2022-02-16 16:45:00 +0800

    autosave leet

---
+++ go/dup1_1.go
@@ -0,0 +1,22 @@
+package main
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+     counts := make(map[string]int)
+     input := bufio.NewScanner(os.Stdin)
+     for input.Scan() {
+         counts[input.Text()]++
+         if input.Text == "\n" {
+            break;
+         }
+     }
+     for line, n := range counts {
+        if n > 1 {
+           fmt.Printf("%d\t%s",n,line)
+        }
+     }
+}

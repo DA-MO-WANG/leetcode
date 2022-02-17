d5439b9f24a354d4647872c173b3f57b74aa7dd5
han guo <guohan741@163.com>
2022-02-17 17:22:00 +0800

    autosave leet

--- go/lissajous.go
+++ go/lissajous.go
@@ -18,7 +18,7 @@ const (
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())a
+	rand.Seed(time.Now().UTC().UnixNano())
 	lissajous(os.Stdout)
 }
 
@@ -37,7 +37,7 @@ func lissajous(out io.Writer) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2 * size + 1, 2 * size + 1)
 		img := image.NewPaletted(rect,palette)
-		for t := 0.0; t < cycles * 2 * math.pi; t += res {
+		for t := 0.0; t < cycles * 2 * math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t * freq + phase)
 			img.SetColorIndex(size + int(x * size + 0.5), size + int(y * size + 0.5),blackIndex)

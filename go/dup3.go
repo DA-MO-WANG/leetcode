e7c4b0fb0fc24c3d89870df4d37e792c7230d31b
han guo <guohan741@163.com>
2022-02-17 17:02:00 +0800

    autosave leet

--- go/dup3.go
+++ go/dup3.go
@@ -6,19 +6,20 @@ import (
 	"os"
 	"strings"
 )
-
+//核心：ioutil.ReadFile(filename)
 func main() {
 	counts := make(map[string]int)
 
 	
 	for _,filename := range os.Args[1:] {
-		//data为字节切片
+		//data为字节切片,读取文件
+		//区分dup2中的io.open()---这里的ioutil.ReadFilef抽象层次更高，一下子都读出来
 		data, err := ioutil.ReadFile(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr,"dup3:%v\n",err)
 			continue;
 		}
-		//strings.Split函数分割，通过换行符来区分一行
+		//strings.Split函数分割，通过换行符来区分一行/再分割
 		for _,line := range strings.Split(data),"\n") {
 			counts[line]++
 		}

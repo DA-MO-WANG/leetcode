933a08bb9fcf02c5b4819929f062cb9d6480ee0d
han guo <guohan741@163.com>
2022-03-06 09:52:00 +0800

    autosave leet

--- cpp/acwing/junior/chafenjuzhen.cpp
+++ cpp/acwing/junior/chafenjuzhen.cpp
@@ -44,7 +44,7 @@ int main() {
     }
     for(int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; ++j) {
-            b[i][j] = b[i - 1][j -1];
+            b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i -1][j -1];
         }
     }
     for(int i = 1; i <= n; i++) {

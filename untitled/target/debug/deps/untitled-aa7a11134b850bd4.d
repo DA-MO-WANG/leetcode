eff95e54f89d34e50b0650fac1bb816a75db84c9
han guo <guohan741@163.com>
2022-03-06 21:01:00 +0800

    autosave leet

---
+++ untitled/target/debug/deps/untitled-aa7a11134b850bd4.d
@@ -0,0 +1,5 @@
+/Users/richheart/IdeaProjects/leetcode/leetcode/untitled/target/debug/deps/untitled-aa7a11134b850bd4: src/main.rs
+
+/Users/richheart/IdeaProjects/leetcode/leetcode/untitled/target/debug/deps/untitled-aa7a11134b850bd4.d: src/main.rs
+
+src/main.rs:
---
+++ untitled/target/debug/untitled.d
@@ -0,0 +1,1 @@
+/Users/richheart/IdeaProjects/leetcode/leetcode/untitled/target/debug/untitled: /Users/richheart/IdeaProjects/leetcode/leetcode/untitled/src/main.rs

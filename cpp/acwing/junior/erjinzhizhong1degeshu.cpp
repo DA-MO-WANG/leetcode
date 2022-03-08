5778f6ec4d35d711ad7ea530586c25cfa519fc95
han guo <guohan741@163.com>
2022-03-08 08:57:00 +0800

    autosave leet

--- cpp/acwing/junior/erjinzhizhong1degeshu.cpp
+++ cpp/acwing/junior/erjinzhizhong1degeshu.cpp
@@ -1,4 +1,23 @@
 //
 // Created by rich heart on 3/8/22.
 //
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while(n--) {
+        int x;
+        cin >> x;
+
+        while (x) x -= lowbit();
+    }
+
+
+    return 0;
+}

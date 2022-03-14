a72fce54297cb2a3ded45d89fac553d5ca4e97c2
han guo <guohan741@163.com>
2022-03-14 22:20:00 +0800

    autosave leet

--- cpp/acwing/junior/qujianhebing.cpp
+++ cpp/acwing/junior/qujianhebing.cpp
@@ -8,11 +8,38 @@
 using namespace std;
 typedef pair<int, int> pii;
 vector<pii> query;
-vector<int> res;
+vector<int> segs;
+vector<pii> merge(vector<int> &segs) {
+    int st = -2e9, ed = -2e9;
+    vector<pii> res;
+    sort(query.begin(),query.end());
+    for(auto item : query) {
+        if(ed < item.first) {
+            //没有交集，替换掉
+            res.push_back({st,ed});
+            st = item.first, ed = item.second;
+
+        }else {
+            ed = max(ed,item.second);
+        }
+    }
+
+    if(ed == -2e9) {
+        res.push_back({st,ed});
+    }
+    return res;
+}
 int n;
 int main() {
     cin >> n;
-    
+    for(int i = 0; i < n; i++) {
+        int l, r;
+        cin >> l >> r;
+        query.push_back({l,r});
+    }
+    vector<pii> res = merge(segs);
+    cout << res.size() << endl;
+
 
 
 

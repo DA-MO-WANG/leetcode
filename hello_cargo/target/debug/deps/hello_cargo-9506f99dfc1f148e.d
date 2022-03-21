1a6dcda3c8820bdad2efb00f9e59ab1735bf27c9
DA-MO-WANG <guohan741@163.com>
2022-03-21 22:03:00 +0800

    autosave leet

---
+++ hello_cargo/target/debug/deps/hello_cargo-9506f99dfc1f148e.d
@@ -0,0 +1,5 @@
+/Users/richheart/IdeaProjects/leetcode/leetcode/hello_cargo/target/debug/deps/hello_cargo-9506f99dfc1f148e.rmeta: src/main.rs
+
+/Users/richheart/IdeaProjects/leetcode/leetcode/hello_cargo/target/debug/deps/hello_cargo-9506f99dfc1f148e.d: src/main.rs
+
+src/main.rs:

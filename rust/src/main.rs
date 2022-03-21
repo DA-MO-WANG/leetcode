6f653c5e0ff98795b95139e2bfe9bacc901bed25
DA-MO-WANG <guohan741@163.com>
2022-03-21 21:49:00 +0800

    autosave leet

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    println!("the nth number.");
-    let n = 10;
-    let m = if n == 1 {
-        2
-    }else if n == 2 {
-        3
-    }else {
-        8
-    };
-    println!("{}",m)
+    println!("Hello world!");
 }

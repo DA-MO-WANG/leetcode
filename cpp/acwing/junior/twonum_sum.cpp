764926b46d2314868bb1750c08c624beed4d03fa
DA-MO-WANG <guohan741@163.com>
2022-03-27 16:02:00 +0800

    autosave leet

--- cpp/acwing/junior/twonum_sum.cpp
+++ cpp/acwing/junior/twonum_sum.cpp
@@ -5,26 +5,29 @@
 #include <cstdio>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 const int N = 1e9 + 10;
 using namespace std;
-int a[N],b[N],res[2];
+char a[N],b[N];
+int res[2];
 
 int main() {
     int n, m, x;
     cin >> n >> m >> x;
 
     while(n--) {
-         scanf('%s',a);
+        scanf("%c",a);
     }
     while(m--) {
-        scanf('%s',b);
+        scanf("%c",b);
     }
-    int i = 0, j = b.size() - 1;
-    while(1) {
+    int i = 0, j = strlen(b) - 1;
+    while(j >= 0 && i < strlen(a)) {
         if(a[i] + b[j] > x) {
             j--;
         }else if(a[i] + b[j] == x) {
             res[0] = i, res[1] = j;
+            break;
         }else {
             i++;
         }

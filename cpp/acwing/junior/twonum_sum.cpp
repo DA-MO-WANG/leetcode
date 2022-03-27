d178bab496efc2886d4fbbe3a61320b355cbbfc0
DA-MO-WANG <guohan741@163.com>
2022-03-27 15:41:00 +0800

    autosave leet

--- cpp/acwing/junior/twonum_sum.cpp
+++ cpp/acwing/junior/twonum_sum.cpp
@@ -1,4 +1,21 @@
 //
 // Created by rich heart on 3/27/22.
 //
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+const int N = 1e9 + 10;
+using namespace std;
+int a[N],b[N];
+int main() {
+    int n, m, x;
+    cin >> n >> m >> x;
+
+    while(n--) {
+         scanf('%')
+    }
+}
+
+
 

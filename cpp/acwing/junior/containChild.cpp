9371fc96c6b504388f2b8a3c47db327187b34cfa
DA-MO-WANG <guohan741@163.com>
2022-03-27 16:34:00 +0800

    autosave leet

--- cpp/acwing/junior/containChild.cpp
+++ cpp/acwing/junior/containChild.cpp
@@ -6,6 +6,26 @@
 #include <cmath>
 
 using namespace std;
+const int N = 1e5 + 10;
+int a[N],b[N];
+int n, m;
 int main() {
-    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        scanf("%d",a+i);
+    }
+    for(int j = 0; j < m; j++) {
+        scanf("%d",b+j)
+    }
+    for(int i = 0, j = 0; i < n;i++) {
+        if(b[i] == a[j]) {
+            j++;
+        }
+    }
+    if(j == m) {
+        cout << "Yes" << endl;
+    }else {
+        cout << "No" << endl;
+    }
+
 }

5c0fe3bb684e8dabf457b8e017183ecb9d6fc914
DA-MO-WANG <guohan741@163.com>
2022-04-02 17:54:00 +0800

    autosave leet

--- cpp/acwing/junior/singlelist.cpp
+++ cpp/acwing/junior/singlelist.cpp
@@ -5,14 +5,25 @@
 #include <cstdio>
 #include <cmath>
 #include <algorithm>
+#include <cstring>
+
+using namespace std;
 //创建了一个结构体
 struct ListNode{
     int val;
     ListNode *next;
     ListNode(int x) : val(x),next(NULL){}
 };
+int m;
 int main() {
-    
+    ListNode *head = null;
+    cin >> m;
+    while(m--) {
+        string s = "";
+        cin >> s;
+        //读取操作指令
 
+        //
+    }
 
 }

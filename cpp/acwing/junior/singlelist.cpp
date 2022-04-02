a63ea278dc7f806486798e94b69188164eaa23f8
DA-MO-WANG <guohan741@163.com>
2022-04-02 17:37:00 +0800

    autosave leet

--- cpp/acwing/junior/singlelist.cpp
+++ cpp/acwing/junior/singlelist.cpp
@@ -1,4 +1,18 @@
 //
 // Created by rich heart on 4/2/22.
 //
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
 
+struct ListNode{
+    int val;
+    ListNode *node;
+    ListNode
+};
+int main() {
+
+
+
+}

c32a6b82b2693e5f05159c3ed714af768138c30d
DA-MO-WANG <guohan741@163.com>
2022-04-10 09:01:00 +0800

    autosave leet

--- cpp/acwing/junior/A826.cpp
+++ cpp/acwing/junior/A826.cpp
@@ -1,4 +1,21 @@
 //
 // Created by rich heart on 4/10/22.
 //
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+    while(m--) {
+        string o;
+        cin >> o;
+        char c = o[0];
+        switch c
+    }
+}

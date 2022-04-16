06329940de0a909bf1dd04e9b4b80ee3317addc1
DA-MO-WANG <guohan741@163.com>
2022-04-16 15:36:00 +0800

    autosave leet

--- cpp/acwing/junior/shuanglianbiao.cpp
+++ cpp/acwing/junior/shuanglianbiao.cpp
@@ -60,7 +60,7 @@ int main() {
         }else if(ope == "D") {
 
             cin >> k;
-            remove(k+1);//对第k个插入的数的理解，最终落实到xiang yi
+            remove(k+1);//对第k个插入的数的理解，最终落实到xiang ying
         }else if(ope == "IL") {
 
             cin >> k >> x;

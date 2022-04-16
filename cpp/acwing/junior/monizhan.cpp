09746e000748333fef723871b6679ed8371a2765
DA-MO-WANG <guohan741@163.com>
2022-04-16 15:54:00 +0800

    autosave leet

--- cpp/acwing/junior/monizhan.cpp
+++ cpp/acwing/junior/monizhan.cpp
@@ -1,4 +1,30 @@
 //
 // Created by rich heart on 4/16/22.
 //
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int N = 1e5 + 10;
+int q[N], top, down;
+int main() {
+    int m;
+    cin >> m;
+    while(m--) {
+        string ope = "";
+        cin >> ope;
+        if(ope == "push") {
+            int x;
+            cin >> x;
+            q[++top] = x;
+        }else if(ope == "pop") {
+            q[top--] = 0;
+        }else if(ope == "empty") {
+            cout <<((top == down) ? "YES" : "NO");
+        }else {
+            cout << q[top];
+        }
+    }
+}
 

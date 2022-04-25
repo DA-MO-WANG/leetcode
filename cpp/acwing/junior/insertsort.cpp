9f38f3cabcfae06ba1da1407fe1be812173f8278
DA-MO-WANG <guohan741@163.com>
2022-04-25 17:24:40 +0800

    autosave leet

--- cpp/acwing/junior/insertsort.cpp
+++ cpp/acwing/junior/insertsort.cpp
@@ -1,4 +1,38 @@
 //
 // Created by rich heart on 4/25/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
+int q[10];
+
+void swap(int i, int j);
+
+void insertSort() {
+    for (int i = 1; i < 10; ++i) {
+        int j = i - 1;
+        while (j >= 0 && q[i] < q[j]) {
+            swap(i,j);
+            j--;
+        }
+    }
+}
+
+void swap(int i, int j) {
+    int temp = q[i];
+    q[i] = q[j];
+    q[j] = temp;
+}
+
+using namespace std;
+
+int main() {
+    int m = 0;
+    cin >> m;
+    for (int i = 0; i < 10; ++i) {
+        scanf("%d",q + i);
+    }
+
+
+}

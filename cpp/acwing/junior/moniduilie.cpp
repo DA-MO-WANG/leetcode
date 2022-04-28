8a7d69f339126abdce75b88dbe2de0b2c575dc6a
DA-MO-WANG <guohan741@163.com>
2022-04-28 20:09:00 +0800

    autosave leet

--- cpp/acwing/junior/moniduilie.cpp
+++ cpp/acwing/junior/moniduilie.cpp
@@ -1,5 +1,20 @@
 //
 // Created by rich heart on 4/28/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+//用数组模拟队列
+using namespace std;
 
+int m;
+int main() {
+    cin >> m;
+    while (m--) {
+        
+    }
+
+
+
+}
 

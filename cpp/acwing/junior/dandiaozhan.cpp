68989ef2e3ac75bf048d12ae08a487984502748c
DA-MO-WANG <guohan741@163.com>
2022-04-28 21:22:00 +0800

    autosave leet

--- cpp/acwing/junior/dandiaozhan.cpp
+++ cpp/acwing/junior/dandiaozhan.cpp
@@ -1,4 +1,21 @@
 //
 // Created by rich heart on 4/28/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+const int N = 1e5 + 10;
+int q[N], tt, m;
+int main() {
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        int x;
+        cin >> x;
+        while(tt && q[tt] >= x) tt--;
+        if(tt) cout << q[tt] << " ";
+        q[++tt] = x;
+    }
+
+}

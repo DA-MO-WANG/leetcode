17145331796426cbdd5b73c328635baf1e5ae8d4
DA-MO-WANG <guohan741@163.com>
2022-05-03 11:56:01 +0800

    autosave leet

--- cpp/acwing/junior/huadongchuangkou.cpp
+++ cpp/acwing/junior/huadongchuangkou.cpp
@@ -10,15 +10,21 @@
 
 using namespace std;
 const int N = 1e5 + 10;
-int e[N], hh = 0, tt = -1;
+//模拟队列的数组，队列是靠hh 和 tt控制的，和模拟用的数组的容量没关系
+int e[N], q[N];
 int main() {
 
     int n,k;
     cin >> n >> k;
-    int q[k];
+    int hh = 0, tt = -1;
     for (int i = 0; i < n; ++i) {
         cin >> e[i];
-        while(hh <= tt && e[i] > q[tt])
+
+        //保持队列和窗口范围的一直
+        if(hh <= tt && i - k + 1 > q[hh]) hh++;
+        while(hh <= tt && e[q[tt]] >= e[i]) tt--;
+        q[++tt] = i;
+        if(i >= k - 1) cout << q[hh] << " ";
     }
 
 

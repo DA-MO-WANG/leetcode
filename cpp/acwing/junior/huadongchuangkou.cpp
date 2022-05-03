f5748ca2a1d6c2e35adb31be67e63278e5d3831b
DA-MO-WANG <guohan741@163.com>
2022-05-03 11:05:00 +0800

    autosave leet

--- cpp/acwing/junior/huadongchuangkou.cpp
+++ cpp/acwing/junior/huadongchuangkou.cpp
@@ -1,21 +1,25 @@
 //
 // Created by rich heart on 5/3/22.
 //
+
 #include <iostream>
 #include <algorithm>
 #include <cstring>
 #include <cstdio>
+#define _CRT_SECURE_NO_WARNINGS
 
 using namespace std;
 const int N = 1e5 + 10;
 int e[N], hh = 0, tt = -1;
 int main() {
+
     int n,k;
     cin >> n >> k;
     int q[k];
-    for (int i = 0; i < ; ++i) {
-        scanf("%d",e + i);
+    for (int i = 0; i < n; ++i) {
+        cin >> e[i];
     }
+    
 
 
 }

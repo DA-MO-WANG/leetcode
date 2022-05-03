a6a91ce043a6d13b0a66c8ee0690fb83b916e7f6
DA-MO-WANG <guohan741@163.com>
2022-05-03 11:00:00 +0800

    autosave leet

--- cpp/acwing/junior/huadongchuangkou.cpp
+++ cpp/acwing/junior/huadongchuangkou.cpp
@@ -1,4 +1,18 @@
 //
 // Created by rich heart on 5/3/22.
 //
+#include <iostream>
+#include <algorithm>
+#include <cstring>
 
+using namespace std;
+const int N = 1e5 + 10;
+int e[N], hh = 0, tt = -1;
+int main() {
+    int n,k;
+    cin >> n >> k;
+    int q[k];
+    while (n--) {
+        
+    }
+}

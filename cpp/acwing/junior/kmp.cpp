43db2c2a11c93fec9b2b9c40871489259a3075e9
DA-MO-WANG <guohan741@163.com>
2022-05-05 10:25:00 +0800

    autosave leet

--- cpp/acwing/junior/kmp.cpp
+++ cpp/acwing/junior/kmp.cpp
@@ -5,9 +5,29 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
-
+const int N = 1e5 + 10, M = 1e6 + 10;
+int n, m, ne[N];
+char p[N], s[M];
 int main() {
-    
+    cin >> n >> p + 1 >> m >> s + 1;
+
+    //创建next[]
+    for (int i = 2, j = 0; i < n; ++i) {
+        while(j && p[i] != p[j + 1]) j = ne[j];
+        if(p[i] == p[j + 1]) j++;
+        ne[i] = j;
+    }
+
+    //开始匹配
+    for (int i = 1, j = 0; i < m; ++i) {
+        while(j && s[i] != p[j + 1]) j = ne[j];
+        if (s[i] == p[j + 1]) j++;
+        if(j == n) {
+            cout << i - n + 1 << " ";
+        }
+
+    }
 }

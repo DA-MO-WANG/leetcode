048d836e420abf01457ddfd3de229d25a1f05630
DA-MO-WANG <guohan741@163.com>
2022-05-06 17:57:00 +0800

    autosave leet

--- cpp/acwing/junior/maxyihuopair.cpp
+++ cpp/acwing/junior/maxyihuopair.cpp
@@ -5,9 +5,24 @@
 #include <cstring>
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 
 using namespace std;
-
+//要
+const int N = 1e5 + 10;
+int q[N];
 int main() {
 
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> q[i];
+    }
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        //先写出暴力解法
+        for (int j = i + 1; j < n; ++j) {
+            res += Math.max(q[i] ^ q[j]);
+        }
+    }
 }

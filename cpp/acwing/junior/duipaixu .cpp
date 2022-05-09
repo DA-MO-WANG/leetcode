afa8695c0c74ebb67e7f091cf632939ae1575616
DA-MO-WANG <guohan741@163.com>
2022-05-09 16:17:01 +0800

    autosave leet

--- cpp/acwing/junior/duipaixu .cpp
+++ cpp/acwing/junior/duipaixu .cpp
@@ -7,19 +7,36 @@
 
 using namespace std;
 const int N = 1e5 + 10;
-int n 。q[N],hp[N];
+int n, m, q[N],hp[N], size;
+void down(int i) {
+    int t = 0;
+    if(q[i * 2] && !q[i * 2 + 1]) t = i * 2;
+    else if(!q[i * 2] && q[i * 2 + 1]) t = i * 2 + 1;
+    else if(q[i * 2] && q[i * 2 + 1]) t = (q[i * 2] < q[i * 2 + 1] ? i * 2 : i * 2 + 1);
+    while(q[i] > q[t]) {
+        swap(q[i],q[t]);
+        down(t);
+    }
+}
+void removeTop() {
+    q[1] = q[size];
+    size--;
+    down(1);
+}
 void heapfy() {
-
+    for (int i = n/2; i >= 1 ; i--) {
+        down(i);
+    }
 }
 int main() {
-    int n, m;
     cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i <= n; ++i) {
+        size++;
         cin >> q[i];
     }
     for (int j = 0; j < m; ++j) {
-        cout << hp[1] << " ";
-
+        cout << q[1] << " ";
+        removeTop();
 
     }
 }

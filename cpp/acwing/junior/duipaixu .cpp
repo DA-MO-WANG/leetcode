0eb7698e1c281fbec70aeb2f09c1923a2c2664ab
DA-MO-WANG <guohan741@163.com>
2022-05-09 16:53:00 +0800

    autosave leet

--- cpp/acwing/junior/duipaixu .cpp
+++ cpp/acwing/junior/duipaixu .cpp
@@ -7,32 +7,31 @@
 
 using namespace std;
 const int N = 1e5 + 10;
-int n, m, q[N], size;
+int n, m, q[N], se;
 void down(int i) {
     int min = i;
-    //如果左边界没过界
-    if(i * 2 <= size && i * 2 + 1 > size && q[i] > q[i * 2]) min = i * 2;//要判断左子节点是否存在，判断的是左子节点是否越界
-    else if(i * 2 + 1 <= size && q[i * 2 + 1] < q[i * 2] && q[i] > q[i * 2 + 1]) min = i * 2 + 1;
-    else if(i * 2 + 1 <= size && q[i * 2] < q[i * 2 + 1] && q[i] > q[i * 2]) min = i * 2;
+    //求三个点中的最小值的三种情况
+    if(i * 2 <= se  && q[i] > q[i * 2]) min = i * 2;//要判断左子节点是否存在，判断的是左子节点是否越界
+    else if(i * 2 + 1 <= se && q[i] > q[i * 2 + 1]) min = i * 2 + 1;
     if(min != i) {
         swap(q[i],q[min]);
         down(min);
     }
 }
 void removeTop() {
-    q[1] = q[size];
-    size--;
+    q[1] = q[se];
+    se--;
     down(1);
 }
 void heapfy() {
-    for (int i = n/2; i >= 1 ; i--) {
+    for (int i = n/2; i ; i--) {
         down(i);
     }
 }
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= n; ++i) {
-        size++;
+        se++;
         cin >> q[i];
     }
     heapfy();

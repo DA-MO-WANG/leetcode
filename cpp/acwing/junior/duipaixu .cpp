080c08215d676c8b9931e954ae7cf40554daff06
DA-MO-WANG <guohan741@163.com>
2022-05-09 16:46:00 +0800

    autosave leet

--- cpp/acwing/junior/duipaixu .cpp
+++ cpp/acwing/junior/duipaixu .cpp
@@ -9,12 +9,14 @@ using namespace std;
 const int N = 1e5 + 10;
 int n, m, q[N], size;
 void down(int i) {
-    int t = 0;
-    if(i * 2 <= size && i * 2 + 1 > size) t = i * 2;//要判断左子节点是否存在，判断的是左子节点是否越界
-    else if(i * 2 <= size && i * 2 + 1 <= size) t = (q[i * 2] < q[i * 2 + 1] ? i * 2 : i * 2 + 1);
-    while(q[i] > q[t]) {
-        swap(q[i],q[t]);
-        down(t);
+    int min = i;
+    //如果左边界没过界
+    if(i * 2 <= size && i * 2 + 1 > size && q[i] > q[i * 2]) min = i * 2;//要判断左子节点是否存在，判断的是左子节点是否越界
+    else if(i * 2 + 1 <= size && q[i * 2 + 1] < q[i * 2] && q[i] > q[i * 2 + 1]) min = i * 2 + 1;
+    else if(i * 2 + 1 <= size && q[i * 2] < q[i * 2 + 1] && q[i] > q[i * 2]) min = i * 2;
+    if(min != i) {
+        swap(q[i],q[min]);
+        down(min);
     }
 }
 void removeTop() {

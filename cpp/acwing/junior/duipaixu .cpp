8ae4cdbede380d5d18e4a20f18ad3f428bf5b52f
DA-MO-WANG <guohan741@163.com>
2022-05-09 16:02:01 +0800

    autosave leet

--- cpp/acwing/junior/duipaixu .cpp
+++ cpp/acwing/junior/duipaixu .cpp
@@ -1,4 +1,25 @@
 //
 // Created by rich heart on 5/9/22.
 //
+#include <algorithm>
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+const int N = 1e5 + 10;
+int q[N],hp[N];
+void heapfy() {
+    
+}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> q[i];
+    }
+    for (int j = 0; j < m; ++j) {
+        cout << hp[1] << " ";
+
+
+    }
+}

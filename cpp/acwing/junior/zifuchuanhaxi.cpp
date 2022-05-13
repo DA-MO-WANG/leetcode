06e5c0328e888e4b817c1044c6ac9e7359432618
DA-MO-WANG <guohan741@163.com>
2022-05-13 17:10:00 +0800

    autosave leet

--- cpp/acwing/junior/zifuchuanhaxi.cpp
+++ cpp/acwing/junior/zifuchuanhaxi.cpp
@@ -1,4 +1,30 @@
 //
 // Created by rich heart on 5/13/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+const int N = 1e5 + 10;
+int n,m;
+char s[N];
+void prefix_hash() {
+    for (int i = 1; s[i]; ++i) {
+        
+    }
+}
+int main() {
+    cin >> n >> m;
+
+    scanf("%s",s+1);
+    while(m--) {
+        int l1, r1, l2, r2;
+        cin >> l1 >> r1 >> l2 >> r2;
+
+    }
+
+
+
+
+}

19c8959d7105e484ccace2c2ab7a1608c80fe36c
DA-MO-WANG <guohan741@163.com>
2022-05-13 17:27:00 +0800

    autosave leet

--- cpp/acwing/junior/zifuchuanhaxi.cpp
+++ cpp/acwing/junior/zifuchuanhaxi.cpp
@@ -6,22 +6,46 @@
 #include <iostream>
 
 using namespace std;
-const int N = 1e5 + 10;
+//一种简便写法技巧
+typedef unsigned long long ull;
+const int N = 1e5 + 10, P = 131;
 int n,m;
-char s[N];
-void prefix_hash() {
-    for (int i = 1; s[i]; ++i) {
-        h
+int h[N],p[N];
+char str[N];
+
+
+void mi() {
+    p[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        p[i] = p[i - 1] * P;
     }
+}
+
+
+//预先把字符串涉及的前缀都存储起来
+void prefix_hash() {
+    h[0] = 0;
+    for (int i = 1; str[i]; ++i) {
+        h[i] = h[i - 1] * P  + str[i];
+    }
+}
+
+int qujianhash(int l, int r) {
+    return h[r] - h[l - 1] * p[r - l + 1];
 }
 int main() {
     cin >> n >> m;
-
-    scanf("%s",s+1);
+    mi();
+    scanf("%s",str+1);
+    prefix_hash();
     while(m--) {
         int l1, r1, l2, r2;
         cin >> l1 >> r1 >> l2 >> r2;
-
+        if(qujianhash(l1,r1) == qujianhash(l2,r2)) {
+            cout << "Yes" << endl;
+        }else {
+            cout << "No" << endl;
+        }
     }
 
 

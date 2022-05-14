016de4f74ff69f49b81cf2706234de138e5299bf
DA-MO-WANG <guohan741@163.com>
2022-05-14 22:19:00 +0800

    autosave leet

--- cpp/acwing/junior/biaodashiqiuzhi.cpp
+++ cpp/acwing/junior/biaodashiqiuzhi.cpp
@@ -1,66 +1,53 @@
-//
-// Created by rich heart on 5/14/22.
-//
 #include <algorithm>
+#include <iostream>
 #include <cstring>
 #include <stack>
-#include <iostream>
 #include <unordered_map>
 
 using namespace std;
 stack<int> num;
 stack<char> op;
-//操作最后一个运算符
-//最后一个数是倒数第二个数，翻过来写
-void eval() {
-    auto b = num.top(); num.pop();
-    auto a = num.top(); num.pop();
-    auto c = op.top(); op.pop();
-    int x;
-    if( c == '+') x = a + b;
-    else if (c == '-') x = a - b;
-    else if (c == '*') x = a * b;
-    else x = a / b;
-    num.push(x);
 
+void eval() {
+    int b = num.top(); num.pop();
+    int a = num.top(); num.pop();
+    char c = op.top(); op.pop();
+    int x = 0;
+    if(c == '+') x = a + b;
+    else if(c == '-') x = a - b;
+    else if(c == '*') x = a * b;
+    else if(c == '/') x = a / b;
+    num.push(x);
 }
 int main() {
-    
-    //先声明下运算符的优先级：用map 绑定符号和优先级
     unordered_map<char,int> pr{{'+',1},{'-',1},{'*',2},{'/',2}};
     string str;
     cin >> str;
 
-    for (int i = 0; str[i]; ++i) {
+    for (int i = 0;str[i]; ++i) {
         auto c = str[i];
-        //如果当前字符是数字，就把完整的操作数放到栈里
-        if (isdigit(c)) {
+        if(isdigit(c)) {
             int x = 0, j = i;
-            //考虑连续数字，多位数，多位数字符串转化成int类型数
-            while(j < str.size() && isdigit(str[j]))
-                x = x * 10 + str[j++] - '0';//另开了一个指针来识别是否是连续数字，每次计算算一个字符，j就++
+            while(j < str.size() && isdigit(str[j])) {
+                x = x * 10 + str[j++] - '0';
+            }
             i = j - 1;
-            num.push(x);//把那个完整的操作数放进栈里
-
+            num.push(x);
         }
-        else if (c == '(') op.push(c);
-        else if (c == ')') {
-            //把栈里的数都拿出来操作一下
-            //eval 用末尾的操作符，操作末尾的两个数
+        else if(c == '(') op.push(c);
+        else if(c == ')') {
             while(op.top() != '(') eval();
-            //弹出'（'
             op.pop();
         }
-        //普通运算符的情况
         else {
-            //如果运算符栈是空的，且运算符栈顶的优先级> 当前运算符的优先级
-            while(op.size() && pr[op.top()] >= pr[c]) eval();//操作下栈顶元素
+            while(op.size() && pr[op.top()] >= pr[c]) eval();
             op.push(c);
         }
-
     }
-    //把没有操作过的运算符从右往左操作一遍
     while(op.size()) eval();
-    cout << num.top() <<endl;
-    return 0;
+    cout << num.top() << endl;
+
+
+
+
 }

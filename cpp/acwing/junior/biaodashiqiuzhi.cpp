f9c0723f51418cc61abc8486715551a44b181ede
DA-MO-WANG <guohan741@163.com>
2022-05-14 14:20:00 +0800

    autosave leet

--- cpp/acwing/junior/biaodashiqiuzhi.cpp
+++ cpp/acwing/junior/biaodashiqiuzhi.cpp
@@ -1,4 +1,23 @@
 //
 // Created by rich heart on 5/14/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <iostream>
+#include <unordered_map>
 
+using namespace std;
+
+int main() {
+    
+    //先声明下运算符的优先级
+    unordered_map<char,int> pr{{'+',1},{'-',1},{'*',2},{'/',2}};
+    string str;
+    cin >> str;
+
+    for (int i = 0; str[i]; ++i) {
+        auto c = str[i];
+        
+    }
+}

497e8f88ef21a5fa6e3c9d5f384544872d2e5f3e
DA-MO-WANG <guohan741@163.com>
2022-05-22 10:07:00 +0800

    autosave leet

--- cpp/acwing/junior/shudezhongxin.cpp
+++ cpp/acwing/junior/shudezhongxin.cpp
@@ -9,7 +9,7 @@ using namespace std;
 const int N = 1e5 + 10, M = 2 * N;
 int n;
 //一开始默认最大值的最小值是所有节点数量，然后让这个初始状态去和流程中的每一个候选人比较，留下较小的
-int ans = n;
+int ans;
 //有向图的dfs模型
 //第一层：找到把一个点删除后，连通块中的最大值——》各个连通块中的节点数量
 //第二层：对应每一个被删除的点，对应连通块最大节点数量的最小值
@@ -57,6 +57,7 @@ int dfs(int u) {
 int main() {
     cin >> n;
     //默认无向图中每个节点都是孤立的
+    ans = n;
     memset(h,-1,sizeof h);
     int m = n - 1;
     while(m--) {

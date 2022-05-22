8bc39ec2ec880292da8bee8ac892c3e9a97a8090
DA-MO-WANG <guohan741@163.com>
2022-05-22 20:33:00 +0800

    autosave leet

--- cpp/acwing/junior/shuzhongdiandecengci.cpp
+++ cpp/acwing/junior/shuzhongdiandecengci.cpp
@@ -1,4 +1,19 @@
 //
 // Created by rich heart on 5/22/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+const int N = 1e5 + 10;
+//bsf框架打出来:当第一次找到这个点时，就是找到最短距离的时候
+//有向图结构
+int h[N],e[N],ne[N],idx;
+int n,m;
+int main() {
+    cin >>n >>m;
+    
+
+
+}

7f77089d609d8b2f0ab97b293d936ef84b0ec938
DA-MO-WANG <guohan741@163.com>
2022-05-22 20:53:00 +0800

    autosave leet

--- cpp/acwing/junior/shuzhongdiandecengci.cpp
+++ cpp/acwing/junior/shuzhongdiandecengci.cpp
@@ -25,6 +25,8 @@ void bfs(int u) {
     while(hh <= tt) {
         //拿到队头的点--一层层扩展，队列中元素是按层排布的
         int t = q[hh++];//h[t]是单链表上的头节点;t是节点编号
+
+        //顺着当前节点的关系往下走
         for (int i = h[t]; i != -1; i = ne[i]) {
            if(!str[e[i]]) {
                d[e[i]] = d[t] + 1;

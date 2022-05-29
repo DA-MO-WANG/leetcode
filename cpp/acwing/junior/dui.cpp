b6eda784e34ce2f29d641ec146e578cd3a5e1699
DA-MO-WANG <guohan741@163.com>
2022-05-29 08:19:00 +0800

    autosave leet

--- cpp/acwing/junior/dui.cpp
+++ cpp/acwing/junior/dui.cpp
@@ -10,14 +10,20 @@ using namespace std;
 const int N = 1010, M = 110;
 int m, n;
 int v[N];
+//检查堆，主要是否维护了堆相对于二叉树的多出来的定义
+//主要涉及了下沉过程（基于dfs，一扎到底的过程）
 bool checkMaxHeap(int x) {
 
     int max = x;
     int lc =  2 * x, rc = 2 * x + 1;
+    //判断三个点的最大值，只用两次比较
+    //比较顺序，从最小的情况开始
     if(lc <= n && v[x] < v[lc]) max = lc;
     if(rc <= n && v[max] < v[rc]) max = rc;
     if(max != x) {
-        swap(v[max],v[x]);
+        //凡是判断逻辑的，不能更改原数据的结构
+        //这里跟下沉过程还是有点区别的
+        //swap(v[max],v[x]);
         return false;
     }else {
         return checkMaxHeap(lc) && checkMaxHeap(rc);
@@ -41,7 +47,8 @@ bool checkMinHeap(int x) {
 
 }
 //所谓后序遍历、中序遍历，和是什么堆没关系，只根他的二叉树结构有关
-//前、后、中，都是基于dfs，类似栈过程
+//前、后、中，都是基于dfs，类似栈过程--利用递归模拟栈实现
+//!!常忘记递归终止条件——扎到底的时机，最后一层的递归调用代码的执行完毕
 void printPost(int x) {
     if(x > n) return;
     int lc =  2 * x, rc = 2 * x + 1;

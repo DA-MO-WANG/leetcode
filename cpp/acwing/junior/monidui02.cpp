3cf01e5c8f258da18e244bf4d6784e3159d33005
DA-MO-WANG <guohan741@163.com>
2022-05-29 11:44:00 +0800

    autosave leet

--- cpp/acwing/junior/monidui02.cpp
+++ cpp/acwing/junior/monidui02.cpp
@@ -6,11 +6,49 @@
 #include <algorithm>
 
 using  namespace std;
+const int N = 1e5 + 10;
 int n;
-char op[];
+//用数组模拟堆
+int hp[N],size;
+char op[3];
+void down(int index) {
+    if(index > size) return;
+    int min = index;
+    int lc = 2 * index, rc = 2 * index + 1;
+    if(lc <= size && hp[min] > hp[lc]) min = lc;
+    if(rc <= size && hp[min] > hp[rc]) min = rc;
+    if(min != index) swap(hp[min],hp[index]);
+    //?
+    down(lc),down(rc);
+}
+
+void up(int index) {
+    if(index <= 0) return;
+    int f = index / 2;
+    if(hp[f] > hp[index]) swap(hp[f],hp[index]);
+    up(f);
+}
+void add(int x) {
+    hp[++size] = x;
+    up(size);
+}
+void deleteMin() {
+    swap(size,1);
+    size--;
+    
+}
 int main() {
     while (n--) {
-
+        scanf("%s",op);
+        if(op[0] == 'I') {
+            int x;
+            scanf("%d",&x);
+            add(x);
+        }else if(op[0] == 'p') {
+            printf("%d",hp[1]);
+        }else if(op[0] == 'D' && op[1] == 'M') {
+            deleteMin();
+        }
     }
 
 

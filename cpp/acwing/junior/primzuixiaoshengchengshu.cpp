4e0cd5ecd829766a083f157a6c2a1c9b145a293e
DA-MO-WANG <guohan741@163.com>
2022-05-31 07:37:00 +0800

    autosave leet

--- cpp/acwing/junior/primzuixiaoshengchengshu.cpp
+++ cpp/acwing/junior/primzuixiaoshengchengshu.cpp
@@ -1,4 +1,21 @@
 //
 // Created by rich heart on 5/31/22.
 //
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+//prim
+const int N = 510, M = 1e5 + 10;
+int dis[N];//这个东
+int main() {
+
+
+
+
+
+
+
+
+}

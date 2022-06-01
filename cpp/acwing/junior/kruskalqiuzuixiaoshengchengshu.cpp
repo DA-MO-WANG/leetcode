658f21822ee687cf0b476c7d5692dbe15dbf7137
DA-MO-WANG <guohan741@163.com>
2022-06-01 18:09:00 +0800

    autosave leet

--- cpp/acwing/junior/kruskalqiuzuixiaoshengchengshu.cpp
+++ cpp/acwing/junior/kruskalqiuzuixiaoshengchengshu.cpp
@@ -11,14 +11,33 @@ using namespace std;
 //稀疏图——》最小生成树——》kruskal
 const int M = 2e5 + 10;
 int n,m;
-struct edge{
+int q[N];
+struct Edge{
     int u,v,w;
-    //充写一个
-};
+    //重写运载符<
+    bool operator< (const Edge &W)const {
+        return w < W.w;
+    }
+}Edge[M];
 int main() {
-    //第一步：把边权重升序排列
     scanf("%d%d",&n,&m);
-    while (m)
+    //准备用结构体数组来存边
+    for (int i = 0; i < m ; ++i) {
+        int u,v,w;
+        scanf("%d%d%d",&u,&v,&w);
+        Edge[i] = {u,v,w};
+    }
+    //准备并查集部分,初始化
+    for (int k = 1; k <= n; ++k) {
+        q[k] = k;
+    }
+    //第一步：把边权重升序排列
+    sort(Edge);
+    //第二步：枚举
+    for (int j = 0; j < m; ++j) {
+        int u = Edge[j].u, v = Edge[j].v, w = Edge.w;
+        if()
+    }
 
 
 

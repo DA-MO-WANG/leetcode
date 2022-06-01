6732ba60558de8b340375729d1da1991b05cc44c
DA-MO-WANG <guohan741@163.com>
2022-06-01 18:00:00 +0800

    autosave leet

--- cpp/acwing/junior/kruskalqiuzuixiaoshengchengshu.cpp
+++ cpp/acwing/junior/kruskalqiuzuixiaoshengchengshu.cpp
@@ -1,5 +1,27 @@
 //
 // Created by rich heart on 6/1/22.
 //
-#include <>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+//稀疏图——》最小生成树——》kruskal
+const int M = 2e5 + 10;
+int n,m;
+struct edge{
+    int u,v,w;
+    //充写一个
+};
+int main() {
+    //第一步：把边权重升序排列
+    scanf("%d%d",&n,&m);
+    while (m)
+
+
+
+
+}
 

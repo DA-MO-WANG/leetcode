0f8bc16a598f1637ba991469b3306b87c9f790b8
DA-MO-WANG <guohan741@163.com>
2022-06-02 11:51:00 +0800

    autosave leet

--- cpp/acwing/junior/ransefapandingerfentu].cpp
+++ cpp/acwing/junior/ransefapandingerfentu].cpp
@@ -18,6 +18,18 @@ void add(int a, int b) {
     h[a] = idx;
     idx++;
 }
+
+int dfs(int x,int c) {
+    color[x] = c;
+    for (int i = h[x]; i != -1 ; i = ne[i]) {
+        int j = e[i];
+        if(!color[j]) {
+            //相邻层的染色关系
+            if(!dfs(j,3 - c)) return false;
+        }else if(color[j] == c) return false;//相邻层，染色一致，出现奇数环
+    }
+    return true;
+}
 int main() {
     //保存图结构
     //因为染色法采用dfs, 借助出边——》用邻接矩阵来描述边
@@ -31,13 +43,12 @@ int main() {
 
     for(int i = 1; i<= n; i++) {
         //dfs也是一层一层走，只不过是单条路径一层层
-        if(!dfs(i,1)) 
-
-
-
-
-
+        if(!dfs(i,1)) {
+            printf("No");
+            break;
+        }
     }
+    printf("Yes");
 
 
 

402e2e91a3eea75507c46a794c9813061a2f86a5
DA-MO-WANG <guohan741@163.com>
2022-06-02 11:35:01 +0800

    autosave leet

--- cpp/acwing/junior/ransefapandingerfentu].cpp
+++ cpp/acwing/junior/ransefapandingerfentu].cpp
@@ -6,9 +6,38 @@
 #include <cstring>
 
 using namespace std;
-in
+const int N = 1e5 + 10, M = 1e5 + 10;
+int h[N],e[M],ne[M],idx;
+int n, m;
+int color[N];//颜色数组
+//无向图——判断二分图——染色法
+void add(int a, int b) {
+    //头插法
+    e[idx] = b;
+    ne[idx] = h[a];
+    h[a] = idx;
+    idx++;
+}
 int main() {
+    //保存图结构
+    //因为染色法采用dfs, 借助出边——》用邻接矩阵来描述边
+    memset(h,-1,sizeof h);
+    while (m--) {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        //无向图——》双向边都要准备
+        add(u,v),add(v,u);
+    }
 
+    for(int i = 1; i<= n; i++) {
+        //dfs也是一层一层走，只不过是单条路径一层层
+        if(!dfs(i,1)) 
+
+
+
+
+
+    }
 
 
 

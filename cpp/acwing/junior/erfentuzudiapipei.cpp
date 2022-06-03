a81d33964265e365d97d82cf16ebec0ebbab986b
DA-MO-WANG <guohan741@163.com>
2022-06-03 18:11:00 +0800

    autosave leet

--- cpp/acwing/junior/erfentuzudiapipei.cpp
+++ cpp/acwing/junior/erfentuzudiapipei.cpp
@@ -14,13 +14,12 @@ bool str[N];
 int n1, n2, m;
 //询问男生x能否找到女生
 int find(int x) {
-    //每次新的男生寻找女生时，都是可以被考虑
-    memset(str,false ,sizeof str);
-    for(int i = h[x]; i != -1; i = ne[i]) {
+    for(int i   = h[x]; i != -1; i = ne[i]) {
         int j = e[i];
-        if(!str[j]) {//如果女生没有被重复考虑
+        if(!str[j]) {//一个男生，不用对同一个女生考虑多次，对一个女生只考虑一次
             str[j] = tr ue;
             if(metch[j] == 0 || find(metch[j])) {
+                metch[j] = x;//更新匹配
                 return true;
             }
         }
@@ -39,6 +38,8 @@ int main() {
     int res = 0;//记录匹配数量
 
     for(int i = 1; i <= n1; i++) {
+        //每次新的男生寻找女生时，都是可以被考虑
+        memset(str,false ,sizeof str);
         if(find(i)) res++;
     }
     printf(res);

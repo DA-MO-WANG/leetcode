fceb75e8c1e3cb12bc924dc60dc2222efef4c607
DA-MO-WANG <guohan741@163.com>
2022-06-04 17:04:00 +0800

    autosave leet

--- cpp/acwing/junior/shifoushizhishu.cpp
+++ cpp/acwing/junior/shifoushizhishu.cpp
@@ -7,16 +7,22 @@
 
 using namespace std;
 int n;
+
+bool is_prime(int a) {
+    if(a <= 2) return false;
+    for (int i = 2; i < a / i; ++i) {
+        if(a % i == 0) return false;
+    }
+    return true;
+}
 int main() {
     cout << n;
     while(n--) {
-
+        int a;
+        cin >> a;
+        bool res = is_prime(a);
+        if(res) cout << "Yes";
+        else cout << "No";
     }
-
-
-
-
-
-
 }
 

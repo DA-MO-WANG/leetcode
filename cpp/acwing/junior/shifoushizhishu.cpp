1f4a0ff443bf905d2c2cbebcddcaf71fc1a39965
DA-MO-WANG <guohan741@163.com>
2022-06-04 16:57:00 +0800

    autosave leet

--- cpp/acwing/junior/shifoushizhishu.cpp
+++ cpp/acwing/junior/shifoushizhishu.cpp
@@ -1,4 +1,22 @@
 //
 // Created by rich heart on 6/4/22.
 //
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int n;
+int main() {
+    cout << n;
+    while(n--) {
+
+    }
+
+
+
+
+
+
+}
 

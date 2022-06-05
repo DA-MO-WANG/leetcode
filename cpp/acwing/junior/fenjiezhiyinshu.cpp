052104501bfe7791ad913255cbeec686a0d1c6a7
DA-MO-WANG <guohan741@163.com>
2022-06-05 08:22:00 +0800

    autosave leet

--- cpp/acwing/junior/fenjiezhiyinshu.cpp
+++ cpp/acwing/junior/fenjiezhiyinshu.cpp
@@ -1,4 +1,35 @@
 //
 // Created by rich heart on 6/5/22.
 //
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+
+int n;
+void fj(int n) {
+    for (int i = 2; i <= n / i; ++i) {
+        if(n % i == 0) {
+            int count = 0;
+            while(n % i == 0) {
+                n /= i;
+                count++;
+            }
+            cout << i << " " << count << endl;
+        }
+    }
+    cout << endl;
+}
+int main() {
+    cin >> n;
+    while(n--) {
+        int a;
+        cin >> a;
+        fj(a);
+    }
+
+
+
+
+
+}

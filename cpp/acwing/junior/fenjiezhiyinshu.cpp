67572c175f063915ece0fb85de1a3234105f02c7
DA-MO-WANG <guohan741@163.com>
2022-06-05 08:29:00 +0800

    autosave leet

--- cpp/acwing/junior/fenjiezhiyinshu.cpp
+++ cpp/acwing/junior/fenjiezhiyinshu.cpp
@@ -8,17 +8,18 @@ using namespace std;
 
 int n;
 void fj(int n) {
-    for (int i = 2; i <= n / i; ++i) {
-        if(n % i == 0) {
+    for (int i = 2; i <= n; ++i) {
+        //要找质因数，从和他相关的约数找，约数中的质数，就是质因子
+        if(n % i == 0) {//===》i是n的约数
             int count = 0;
-            while(n % i == 0) {
+            while(n % i == 0) {//质因数肯定在非质因数的约数组合的合数的约数的前面
                 n /= i;
                 count++;
             }
             cout << i << " " << count << endl;
         }
     }
-    if(n > 1) cout << n << " " << 1 << endl;
+    //if(n > 1) cout << n << " " << 1 << endl;
     cout << endl;
 }
 int main() {

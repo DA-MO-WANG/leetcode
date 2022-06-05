5ddb55c632004fea3fe55a4c66b155c5e9a3ef8b
DA-MO-WANG <guohan741@163.com>
2022-06-05 19:39:00 +0800

    autosave leet

--- cpp/acwing/junior/qiuyueshu.cpp
+++ cpp/acwing/junior/qiuyueshu.cpp
@@ -5,10 +5,36 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-
+int n;
+vector<int> yueshu(int n) {
+    //直接打印法
+    vector<int> res;
+    for (int i = 1; i <= n / i; ++i) {
+        if(n % i == 0) {
+            res.push_back(i);
+            if(i != n / i) res.push_back(n/i);
+        }
+    }
+    return res;
+}
 int main() {
+    cin >> n;
+    while(n--) {
+        int a;
+        cin >> a;
+        vector<int> res = yueshu(a);
 
+        sort(res.begin(),res.end());
+        for (int i = 0; i < res.size(); ++i) {
+            cout << res[i] <<" ";
+        }
+        cout << endl;
+
+
+
+    }
     return 0;
 }

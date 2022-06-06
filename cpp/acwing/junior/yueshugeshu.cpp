05d4461b57bfabdbc62cd747c508ef5c2068bcc7
DA-MO-WANG <guohan741@163.com>
2022-06-06 20:47:00 +0800

    autosave leet

--- cpp/acwing/junior/yueshugeshu.cpp
+++ cpp/acwing/junior/yueshugeshu.cpp
@@ -8,13 +8,34 @@
 #include <unordered_map>
 
 using namespace std;
-const int mod = 1e9 + 7;
-int n;
+const int N = 110,mod = 1e9 + 7;
 
+int n;
+bool str[N];
+unordered_map<int,int> primes;
+void yueshugeshu(int n) {
+    for (int i = 1; i <=n ; ++i) {
+        if(n % i == 0) {
+            if(!str[i]) primes[i]++;
+        }
+        for (int j = i + i; j <=n; j += i) {
+            str[j] = true;
+        }
+    }
+}
 int main() {
     cin >> n;
-    for (int i = 1; i <=n ; ++i) {
+
+    while (n--) {
 
     }
+
+    int res = 1;
+    for (auto prime : primes) {
+        int p = prime.first, k = prime.second;
+        int t = 1;
+        t = p * t + 1;
+    }
+    res = res * t;
     return 0;
 }

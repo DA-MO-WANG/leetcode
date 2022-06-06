add470ae33f76d630b28f6c6aaa9988c6f08f9eb
DA-MO-WANG <guohan741@163.com>
2022-06-06 20:37:00 +0800

    autosave leet

--- cpp/acwing/junior/yueshugeshu.cpp
+++ cpp/acwing/junior/yueshugeshu.cpp
@@ -5,10 +5,16 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
+const int mod = 1e9 + 7;
+int n;
 
 int main() {
+    cin >> n;
+    for (int i = 1; i <=n ; ++i) {
 
+    }
     return 0;
 }

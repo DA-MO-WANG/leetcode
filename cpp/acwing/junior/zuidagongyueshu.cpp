2f453a5815a3af0c86da8b826f06f627c89c3f56
DA-MO-WANG <guohan741@163.com>
2022-06-07 17:42:01 +0800

    autosave leet

--- cpp/acwing/junior/zuidagongyueshu.cpp
+++ cpp/acwing/junior/zuidagongyueshu.cpp
@@ -7,8 +7,17 @@
 #include <algorithm>
 
 using namespace std;
-
+int n;
+int gcd(int a, int b) {
+    return b ? (b, a % b) : a;
+}
 int main() {
-
+    cin >> n;
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+        int res = gcd(a,b);
+        cout << res << endl;
+    }
     return 0;
 }

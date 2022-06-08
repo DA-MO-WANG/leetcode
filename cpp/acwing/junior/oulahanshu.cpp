c083df7d0863e54f6e5d982a46d9c15736ddeec2
DA-MO-WANG <guohan741@163.com>
2022-06-08 11:56:00 +0800

    autosave leet

--- cpp/acwing/junior/oulahanshu.cpp
+++ cpp/acwing/junior/oulahanshu.cpp
@@ -9,16 +9,20 @@
 using namespace std;
 int n;
 int oula(int n) {
+    int n1 = n;
     //先求出x的质因数
     unordered_map<int,int> primes;
-    for(int i = 1; i <= n /i; i++) {
+    //1 不算质因子，从2开始
+    //因为最多只有一个大于sqrt n的，所以i <= n /i
+    for(int i = 2; i <= n /i; i++) {
         while(n % i == 0) {
             n /= i;
             primes[i]++;
         }
     }
-
-    int res = n;
+    //应对最后一种情况
+    if(n > 1) primes[n]++;
+    int res = n1;
     for (auto prime : primes) {
         int p = prime.first;
         res = res / p * (p - 1);

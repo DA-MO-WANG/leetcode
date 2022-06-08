02a0ef6bf757c5e1c348615bf922ca7e4f7439d2
DA-MO-WANG <guohan741@163.com>
2022-06-08 11:25:00 +0800

    autosave leet

--- cpp/acwing/junior/oulahanshu.cpp
+++ cpp/acwing/junior/oulahanshu.cpp
@@ -5,10 +5,33 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+#include <unordered_map>
 using namespace std;
+int n;
+int oula(int n) {
+    //先求出x的质因数
+    unordered_map<int,int> primes;
+    for(int i = 1; i <= n /i; i++) {
+        while(n % i == 0) {
+            n /= i;
+            primes[i]++;
+        }
+    }
 
+    int res = n;
+    for (auto prime : primes) {
+        int p = prime.first;
+        res = res / p * (p - 1);
+    }
+    return res;
+}
 int main() {
-
+    cin >> n;
+    while (n--) {
+        int a;
+        cin >> a;
+        int res = oula(a);
+        cout << res;
+    }
     return 0;
 }

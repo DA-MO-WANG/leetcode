f310aa46be34fee2d3b7bc0af4302ecac9aae11c
DA-MO-WANG <guohan741@163.com>
2022-06-08 18:20:00 +0800

    autosave leet

--- cpp/acwing/junior/shaifaqiuoulahanshu.cpp
+++ cpp/acwing/junior/shaifaqiuoulahanshu.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 //1-n中每个数的欧拉函数之和
 using namespace std;
+typedef long long LL;
 const int N = 1e6 + 10;
 int n;
 //不要nsqrtn的复杂度==》线性筛法
@@ -15,15 +16,26 @@ bool str[N];
 int primes[N],cnt;
 
 int sfoula(int n) {
+    phi[1] = 1;
     for (int i = 2; i < n; ++i) {
         if(!str[i]) {
             primes[cnt++] = i;
+            phi[i] = i - 1;
         }
         for (int j = 0; primes[j] <= n / i ; ++j) {
             str[primes[j] * i] = true;
-            if(i % primes[j] == 0) break;
+            if(i % primes[j] == 0) {
+                phi[primes[j] * i] = phi[i] * phi[j];
+                break;
+            }
+            phi[primes[j] * i] = phi[i] * (primes[j] - 1);
         }
     }
+    LL res = 0;
+    for (int k = 0; k < n; ++k) {
+        res += phi[i];
+    }
+    return res;
 }
 
 int main() {

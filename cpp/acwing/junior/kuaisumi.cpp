51c6954d088e9efcaffc01bf699529045e4aafd5
DA-MO-WANG <guohan741@163.com>
2022-06-20 17:33:00 +0800

    autosave leet

--- cpp/acwing/junior/kuaisumi.cpp
+++ cpp/acwing/junior/kuaisumi.cpp
@@ -9,11 +9,18 @@
 using namespace std;
 int n;
 typedef long long ll;
+int qmi(int a, int b, int k) {
+    int res = 1;
+    while(k) {
+        if(k & 1) res = res * a % k
+    }
+}
 int main() {
     cin >> n;
     while(n--) {
         int a, b , k;
-        scanf("%d%d%d",&a,&b,&k)
+        scanf("%d%d%d",&a,&b,&k);
+        printf("%d",qmi(a,b,k));
     }
     return 0;
 }

c80d8f6905415f47bb4c5e84850fedae7e49f367
DA-MO-WANG <guohan741@163.com>
2022-06-28 16:24:00 +0800

    autosave leet

--- cpp/acwing/junior/kuozhanoujilidesuanfa.cpp
+++ cpp/acwing/junior/kuozhanoujilidesuanfa.cpp
@@ -8,11 +8,14 @@
 
 using namespace std;
 int n;
+//返回最大公约数
 int exgcd(int a, int b, int &x, int &y) {
+    //递归基条件
     if(!b) {
         x = 1, y = 0;
         return a;
     }
+    //y x 倒过来填放，方便后续计算
     int d = exgcd(b, a % b, y , x);
     y = y - a / b * x;
     return d;

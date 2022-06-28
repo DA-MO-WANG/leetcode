7c3b07ef6bf9851a47219803160d17717bc76ff6
DA-MO-WANG <guohan741@163.com>
2022-06-28 16:20:01 +0800

    autosave leet

--- cpp/acwing/junior/kuozhanoujilidesuanfa.cpp
+++ cpp/acwing/junior/kuozhanoujilidesuanfa.cpp
@@ -7,8 +7,23 @@
 #include <algorithm>
 
 using namespace std;
-
+int n;
+int exgcd(int a, int b, int &x, int &y) {
+    if(!b) {
+        x = 1, y = 0;
+        return a;
+    }
+    int d = exgcd(b, a % b, y , x);
+    y = y - a / b * x;
+    return d;
+}
 int main() {
-
+    cin >> n;
+    while(n--) {
+        int a, b, x, y;
+        scanf("%d%d",&a,&b);
+        exgcd(a,b,x,y);
+        printf("%d%d\n",x,y);
+    }
     return 0;
 }

400ff5c2a83eaaf451855c277d77c06376426b4f
DA-MO-WANG <guohan741@163.com>
2022-06-30 19:51:00 +0800

    autosave leet

---
+++ java/src/acwing/Main.java
@@ -0,0 +1,8 @@
+package acwing;/**
+ * @Description TO DO
+ * @Classname Main
+ * @Date 2022/6/30 19:50
+ * @Created by richheart
+ * Copyright (c) All Rights Reserved, 2022.
+ */public class Main {
+}

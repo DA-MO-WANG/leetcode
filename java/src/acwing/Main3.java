3dcd93ce210466ddbfad38b74585476074c4048e
DA-MO-WANG <guohan741@163.com>
2022-07-01 11:04:01 +0800

    autosave leet

--- java/src/acwing/Main3.java
+++ java/src/acwing/Main3.java
@@ -16,17 +16,17 @@ public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.next();
-        //char[] letters = input.toCharArray();
-//        int[] cnt = new int[100];
-//        for (int i = 0; i < letters.length; i++) {
-//            cnt[letters[i] - 'a']++;
-//        }
-//        for (int i = 0; i < 100; i++) {
-//            if(cnt[i] == 1) {
-//                System.out.println('a' + i);
-//            }
-//        }
+        String input = sc.nextLine();
+        char[] letters = input.toCharArray();
+        int[] cnt = new int[100];
+        for (int i = 0; i < letters.length; i++) {
+            cnt[letters[i] - 'a']++;
+        }
+        for (int i = 0; i < 100; i++) {
+            if(cnt[i] == 1) {
+                System.out.println('a' + i);
+            }
+        }
 
 
 

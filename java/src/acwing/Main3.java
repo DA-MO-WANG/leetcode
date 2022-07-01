62776c5b82d6d2eef651470050e1e3d63ee042a3
DA-MO-WANG <guohan741@163.com>
2022-07-01 10:39:00 +0800

    autosave leet

--- java/src/acwing/Main3.java
+++ java/src/acwing/Main3.java
@@ -1,8 +1,25 @@
-package acwing;/**
+package acwing;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
  * @Description TO DO
  * @Classname Main3
  * @Date 2022/7/1 10:28
  * @Created by richheart
  * Copyright (c) All Rights Reserved, 2022.
- */public class Main3 {
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] words = input.toCharArray();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i],0
+        }
+
+
+    }
 }

f91a964d88446dcaa92e3172be18df137c874bf7
DA-MO-WANG <guohan741@163.com>
2022-07-01 10:56:00 +0800

    autosave leet

--- java/src/acwing/Main3.java
+++ java/src/acwing/Main3.java
@@ -16,19 +16,17 @@ public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        char[] words = input.toCharArray();
-        HashMap<Character,Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            if(!map.containsKey(words[i])) {
-                map.put(words[i],1);
-            }else {
-                map.put(words[i],2);
+        char[] letters = input.toCharArray();
+        int[] cnt = new int[100];
+        for (int i = 0; i < letters.length; i++) {
+            cnt[letters[i] - 'a']++;
+        }
+        for (int i = 0; i < 100; i++) {
+            if(cnt[i] == 1) {
+                System.out.println('a' + i);
             }
         }
-        Set<Map.Entry<Character, Integer>> entries = map.entrySet();
-        for (Map.Entry<Character, Integer> entry : entries) {
-            if(entry.getValue() == 1)
-        }
+
 
 
     }

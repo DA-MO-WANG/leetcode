494ae55765da6b20d9a84287076dbffcb23ef520
DA-MO-WANG <guohan741@163.com>
2022-07-04 22:32:00 +0800

    autosave leet

--- cpp/acwing/junior/linzhichazhao.cpp
+++ cpp/acwing/junior/linzhichazhao.cpp
@@ -1,15 +1,19 @@
 //
 // Created by rich heart on 04/07/2022.
 //
-//y总思路
+//y总思路:利用链表来做
 //审题意
-//
+//对于每一个数，找到他左边范围中与他值距离最近的距离，及对应数的下标
+//因为结果中要下标——》保存原来的值与下标的绑定关系——》pair队
+//因为左边范围，所以倒着算，可以利用排序
+//排序又不能破坏原来的下标——》pair
 #include <cstring>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
-
+typedef long long ll;
+pair<ll,int>
 int main() {
 
     return 0;

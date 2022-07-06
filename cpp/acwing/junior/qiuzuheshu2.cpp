ad8b669374041ba6fe3b20e43d77980f1b6288f8
DA-MO-WANG <guohan741@163.com>
2022-07-06 16:43:00 +0800

    autosave leet

--- cpp/acwing/junior/qiuzuheshu2.cpp
+++ cpp/acwing/junior/qiuzuheshu2.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 typedef long long ll;
-const int N = 1e5 + 10, mod = 1e9 + 10;
+const int N = 1e5 + 10, mod = 1e9 + 7;
 int fact[N], infact[N];
 int n;
 int qmi(int a, int k, int p) {
@@ -23,14 +23,14 @@ int qmi(int a, int k, int p) {
 int main() {
     fact[0] = infact[0] = 1;
     for(int i = 1; i < N; i++) {
-       fact[i] = fact[i - 1] * i % mod;
-       infact[i] = infact[i - 1] * qmi(i , mod - 2, mod) % mod;
+       fact[i] = (ll)fact[i - 1] * i % mod;
+       infact[i] = (ll)infact[i - 1] * qmi(i , mod - 2, mod) % mod;
     }
     cin >> n;
     while(n--) {
         int a, b;
         scanf("%d%d",&a,&b);
-        printf("%d\n",(ll)fact[a] * infact[b-a] * infact[b] % mod);
+        printf("%d\n",(ll)fact[a] * infact[b-a] % mod * infact[b] % mod);
     }
     return 0;
 }

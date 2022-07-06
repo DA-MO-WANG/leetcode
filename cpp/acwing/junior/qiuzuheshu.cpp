84499a8067a41d5f126add20a6e3b40f93c85984
DA-MO-WANG <guohan741@163.com>
2022-07-06 15:37:00 +0800

    autosave leet

--- cpp/acwing/junior/qiuzuheshu.cpp
+++ cpp/acwing/junior/qiuzuheshu.cpp
@@ -7,7 +7,8 @@
 #include <algorithm>
 
 using namespace std;
-const int N = 2010;
+typedef long long ll;
+const int N = 2010, mod = 1e9 + 10;
 int c[N][N];
 int n;
 void init() {
@@ -16,7 +17,7 @@ void init() {
         for(int j = 0; j <= i; j++) {
             if(!j) c[i][j] = 1;
             // c[0][1]这种视为0，不参与计算，不合法的组合数，不处理就是0
-            else c[i][j] = c[i - 1][j] + c[i - 1][j - 1];//要考虑c00 = 1 如果i从1开始，就会把c00 当成0
+            else c[i][j] = ll(c[i - 1][j] + c[i - 1][j - 1]) % mod;//要考虑c00 = 1 如果i从1开始，就会把c00 当成0
         }
     }
 }

1318912e77d24bc0a9e683d34ec702bd0065d3b4
DA-MO-WANG <guohan741@163.com>
2022-07-09 19:19:00 +0800

    autosave leet

--- cpp/acwing/junior/quanpailie2.cpp
+++ cpp/acwing/junior/quanpailie2.cpp
@@ -2,13 +2,18 @@
 // Created by rich heart on 09/07/2022.
 //
 
-#include <cstring>
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-int main() {
-
-    return 0;
-}
+class Solution {
+public:
+    //准备一个存放最终结果的结构
+    vector<vector<int>> ans;
+    //准备一个存放一种方案情况
+    vector<int> path;
+    vector<vector<int>> permutation(vector<int>& nums) {
+        //设计几个关键变量，为了达成一定目的
+        //u==>记录使用了几个数
+        //start==>记录上一个相同的数存放的位置
+        //state==>用来记录当前位置上的空缺信息
+        dfs(nums,0,0,0);
+    }
+    void dfs(int )
+};

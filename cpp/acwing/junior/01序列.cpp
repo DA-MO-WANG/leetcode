0a53b2897cc2988f789ced5a13720eca7581e92d
DA-MO-WANG <guohan741@163.com>
2022-07-09 12:02:00 +0800

    autosave leet

--- cpp/acwing/junior/01序列.cpp
+++ cpp/acwing/junior/01序列.cpp
@@ -7,12 +7,34 @@
 #include <algorithm>
 
 using namespace std;
+typedef long long ll;
+const int N = 1e5 + 10, mod = 1e9 + 7;
+int fact[N], infact[N];
 int n;
+int qmi(int a, int k, int p) {
+    int res = 1;
+    while(k) {
+        if(k & 1) res = (ll)res * a % p;
+        a = (ll)a * a % p;
+        k >>= 1;
+    }
+    return res;
+}
 int main() {
     cin >> n;
-        
-
-
-
+    //factn 代表n的阶乘
+    //infact n 代表n阶乘的逆元
+    fact[0] = infact[0] = 1;
+    for(int i = 1; i < 2*N; i++) {
+        fact[i] = (ll)fact[i - 1] * i % mod;
+        infact[i] = (ll)infact[i - 1] * qmi(i , mod - 2, mod) % mod;
+    }
+    
+    while(n--) {
+        int a, b;
+        scanf("%d%d",&a,&b);
+        int res = (ll)fact[a] * infact[a - b] % mod * infact[b] % mod;
+        printf("%d\n",res / (n + 1);
+    }
     return 0;
 }

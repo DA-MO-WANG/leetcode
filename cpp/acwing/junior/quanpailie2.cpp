6145e8e0010a2e51b0d5cb961cdda6f7fb325cce
DA-MO-WANG <guohan741@163.com>
2022-07-09 19:23:00 +0800

    autosave leet

--- cpp/acwing/junior/quanpailie2.cpp
+++ cpp/acwing/junior/quanpailie2.cpp
@@ -15,5 +15,18 @@ public:
         //state==>用来记录当前位置上的空缺信息
         dfs(nums,0,0,0);
     }
-    void dfs(int )
+    void dfs(vector<int>& nums, int u, int start, int state) {
+        if(u == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        if(!u || nums[u] != nums[u - 1]) start = 0;
+        for (int i = start; i < nums.size(); ++i) {
+            if(!((state >> i) & 1)) {
+                path[i] = nums[u];
+                dfs(nums,u + 1, i + 1, state + (1 << i));
+            }
+        }
+    }
 };

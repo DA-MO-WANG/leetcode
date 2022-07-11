e4f6b0ce12e77e2392804f0c86b795011061633d
DA-MO-WANG <guohan741@163.com>
2022-07-11 16:48:00 +0800

    autosave leet

--- cpp/acwing/junior/nengbeizhengchudeshu.cpp
+++ cpp/acwing/junior/nengbeizhengchudeshu.cpp
@@ -7,8 +7,30 @@
 #include <algorithm>
 
 using namespace std;
-
+typedef long long ll;
+int q[16];
 int main() {
-
+    int n, m;
+    cin >> n >> m;
+    int res = 1;
+    for (int i = 0; i < m; ++i) cin >> q[i];
+    for (int i = 1; i < 2 >> m; ++i) {
+        ll t = 1, cnt = 0;
+        for (int j = 0; j < m; ++j) {
+            if(i >> j & 1) {
+                cnt++;
+                t = t * q[j];
+                if(t > n) {
+                    t = -1;
+                    break;
+                }
+            }
+        }
+        if(t != -1) {
+            if(cnt % 2) res += n / t;
+            else res -= n / t;
+        }
+    }
+    cout << res << endl;
     return 0;
 }

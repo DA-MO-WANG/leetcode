5e3de8c9a5a6aaa36e14f790ebc0174509deb9e4
DA-MO-WANG <guohan741@163.com>
2022-07-13 15:47:32 +0800

    autosave leet

--- cpp/acwing/junior/01beibaowenti.cpp
+++ cpp/acwing/junior/01beibaowenti.cpp
@@ -7,8 +7,22 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+//记录体积情况、权重情况
+int v[N],w[N];
+//记录状态
+int f[N][N];
 int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d%d",v[i],w[i]);
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= m; ++j) {
+            if(j > v[i]) f[i][j] = f[i - 1][j]
+        }
+    }
 
     return 0;
 }

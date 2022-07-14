55df04e78924462c92ff0bcc140cc4eb7446ac1b
DA-MO-WANG <guohan741@163.com>
2022-07-14 09:38:00 +0800

    autosave leet

--- cpp/acwing/junior/01beibaowenti.cpp
+++ cpp/acwing/junior/01beibaowenti.cpp
@@ -13,6 +13,8 @@ const int N = 1010;
 int v[N],w[N];
 //记录状态
 //int f[N][N];
+
+//滚动数组优化
 int f[N];
 int main() {
     int n, m;
@@ -21,12 +23,13 @@ int main() {
         cin >> v[i] >> w[i];
     }
     for (int i = 1; i <= n; ++i) {
+        //从大到小，是为了记忆留存的效果不被i时的j - v[i] 替换掉
         for (int j = m; j >= v[i]; j--) {
             //f[i][j] = f[i - 1][j];
             f[j] = max(f[j],f[j - v[i]] + w[i]);
         }
     }
-    cout << f[n][m];
+    cout << [m];
 
     return 0;
 }

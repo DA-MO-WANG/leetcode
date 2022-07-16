7d5bf8253c11658c5c8db0bdfb8d245cfcceef78
DA-MO-WANG <guohan741@163.com>
2022-07-16 10:37:00 +0800

    autosave leet

--- cpp/acwing/junior/wanquanbeibaowenti.cpp
+++ cpp/acwing/junior/wanquanbeibaowenti.cpp
@@ -5,10 +5,28 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
-
+const int N = 1010;
+int v[N], w[N];
 int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        cin >> v[i] >> w[i];
+    }
 
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= m; ++j) {
+            f[i][j] = f[i - 1][j];
+            int k = 0;
+            while(j >= k * v[i]) {
+                f[i][j] = max(f[i][j],f[i - 1][j - k * v[i]] + k * w[i]);
+                k++;
+            }
+        }
+    }
+    cout << f[n][m];
     return 0;
 }

7d58df777797bf0db5c447e5ecc2edc24e31ec84
DA-MO-WANG <guohan741@163.com>
2022-07-17 11:47:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao1.cpp
+++ cpp/acwing/junior/duochongbeibao1.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 #include <cmath>
 using namespace std;
-const int N = 1010;
+const int N = 110;
 int v[N],w[N],s[N];
 int f[N][N];
 int n, m;
@@ -19,7 +19,10 @@ int main() {
     for (int i = 1; i <= n ; ++i) {
         for (int j = 0; j <= m ; ++j) {
             for (int k = 0; k <= s[i] && k * v[i] <= j; ++k) {
-                f[i][j] = max(f[i - 1][j], f[i - 1][j - k * v[i]] + k * w[i]);
+                //k = 0 就是选0个的情况
+                //max你得保留一个自身，用来更新，进而反复比较
+                //易错点：这几个公式容易混淆，所以说要明白写的每一个式子都在说什么
+                f[i][j] = max(f[i][j], f[i - 1][j - k * v[i]] + k * w[i]);
             }
         }
     }

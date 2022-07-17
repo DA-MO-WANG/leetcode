101a54713e4371640c6a58f93eedd55649c7e89c
DA-MO-WANG <guohan741@163.com>
2022-07-17 20:24:00 +0800

    autosave leet

--- cpp/acwing/junior/fenzubeibao.cpp
+++ cpp/acwing/junior/fenzubeibao.cpp
@@ -5,10 +5,29 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
-
+int n, m;
+const int N = 110;
+int v[N][N],w[N][N];
+int f[N][N];
 int main() {
-
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        int s;
+        cin >> s;
+        for (int j = 1; j <= s; ++j) {
+            cin >> v[i][j] >> w[i][j];
+        }
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= m ; ++j) {
+            f[i][j] = f[i - 1][j];
+            for (int k = 1; v[i][k]; ++k) {
+                f[i][j] = max(f[i][j],f[i - 1][j - v[i][k]] + w[i][k]);
+            }
+        }
+    }
+    cout << f[n][m] <<endl;
     return 0;
 }

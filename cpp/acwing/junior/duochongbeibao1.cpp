9ef5dd3e62bb113a574bcc236733d270a15237b8
DA-MO-WANG <guohan741@163.com>
2022-07-17 11:36:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao1.cpp
+++ cpp/acwing/junior/duochongbeibao1.cpp
@@ -7,8 +7,15 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+int v[N],w[N],s[N];
+int f[N][N];
+int n, m;
 int main() {
-
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        cin >> v[i] >> w[i] >> s[i];
+    }
+    
     return 0;
 }

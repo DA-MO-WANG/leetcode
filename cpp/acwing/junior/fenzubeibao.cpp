0b64bad8cd43a8ede69554feee69c7b0d3e13189
DA-MO-WANG <guohan741@163.com>
2022-07-17 20:45:01 +0800

    autosave leet

--- cpp/acwing/junior/fenzubeibao.cpp
+++ cpp/acwing/junior/fenzubeibao.cpp
@@ -9,25 +9,24 @@
 using namespace std;
 int n, m;
 const int N = 110;
-int v[N][N],w[N][N];
-int f[N][N];
+int v[N][N],w[N][N],s[N];
+int f[N];
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= n; ++i) {
-        int s;
-        cin >> s;
-        for (int j = 1; j <= s; ++j) {
+        cin >> s[i];
+        for (int j = 1; j <= s[i]; ++j) {
             cin >> v[i][j] >> w[i][j];
         }
     }
     for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j <= m ; ++j) {
-            f[i][j] = f[i - 1][j];
-            for (int k = 1; v[i][k]; ++k) {
-                f[i][j] = max(f[i][j],f[i - 1][j - v[i][k]] + w[i][k]);
-            }
+        for (int j = m; j >= 0; j--) {
+            //循环次序不能颠倒
+            for (int k = 0; k <= s[i]; k++ )
+                if(j >= v[i][k]) f[j] = max(f[j],f[j - v[i][k]] + w[i][k]);
         }
+
     }
-    cout << f[n][m] <<endl;
+    cout << f[m] <<endl;
     return 0;
 }

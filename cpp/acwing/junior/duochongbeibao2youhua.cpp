4bf58219cdd095f4ca988d568fd4761972e233ae
DA-MO-WANG <guohan741@163.com>
2022-07-17 11:31:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao2youhua.cpp
+++ cpp/acwing/junior/duochongbeibao2youhua.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 
 using namespace std;
-const int N = 11000, M = 2010;
+const int N = 11010, M = 2010;
 //开多大空间
 //2000子物品数量，1000物品种类=》每个2000，对应2的11幂，11*1000
 //cnt的限制
@@ -19,22 +19,28 @@ int main() {
     //新物品的编号
     int cnt = 0;
     for (int i = 1; i <= n; ++i) {
+        //a是体积属性、b是价值属性、s是数量
         int a, b ,s;
         cin >> a >> b >> s;
+        //k是分割s的挡板
         int k = 1;
         while(k <= s) {
             cnt ++;
             v[cnt] = a * k;
             w[cnt] = b * k;
+            //更新循环变量
             s -= k;
             k <<= 1;
         }
+        //求c
+        //剩下s数量
         if(s) {
             cnt ++;
-            v[cnt] = a * k;
-            w[cnt] = b * k;
+            v[cnt] = a * s;
+            w[cnt] = b * s;
         }
     }
+    //01背包
     n = cnt;
     for (int i = 1; i <= n; ++i) {
         for (int j = m; j >= v[i] ; j--) {

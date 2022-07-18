7603335bb02e233db05566eeed349416879bc7a5
DA-MO-WANG <guohan741@163.com>
2022-07-18 09:51:00 +0800

    autosave leet

--- cpp/acwing/junior/shuzisanjiaoxing .cpp
+++ cpp/acwing/junior/shuzisanjiaoxing .cpp
@@ -5,10 +5,25 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
-
+const int N = 510;
+int n;
+int f[N][N];
+int v[N][N];
 int main() {
-
+    cin >> n;
+    for (int i = 1; i <= n ; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cin >> v[i][i];
+        }
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            f[i][j] = max(f[i - 1][j - 1] + v[i][j], f[i - 1][j] + v[i][j]);
+        }
+    }
+    cout << f[n][m] << endl;
     return 0;
 }

118cbaba85b91011cb2c16203fc899aa881a0296
DA-MO-WANG <guohan741@163.com>
2022-07-19 11:06:00 +0800

    autosave leet

--- cpp/acwing/junior/zuichangshangshengzixulie.cpp
+++ cpp/acwing/junior/zuichangshangshengzixulie.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <stack>
 
 using namespace std;
 const int N = 1010;
@@ -15,21 +16,31 @@ int v[N];
 //状态的数目取决于分类点的选择
 //这里的分类点是倒数第二个位置---倒数第二个位置的选择区间是[0,i-1]
 int f[N];
-int q[N], tt = -1;
+stack<int> s;
 int main() {
     cin >> n;
     for (int i = 1; i <= n; ++i) {
         cin >> v[i];
         //cout << v[i] << " ";
     }
-    q[++tt] = v[1];
+    q[++tt] = 1;
     //第一层循环遍历的是结尾的情况
     for (int j = 1; j <= n; ++j) {
         //第二层循环遍历的是倒数第二个位置的情况
         //每换一种集合，集合中的第一个初始情况都是这个序列第二位置没有，只有本身一个数的情况，这是下面情况的出发点
         f[j] = 1;
-        while(qq[tt] < v[j]) {
-            
+
+        int cur = tt;
+        while(cur >= 0) {
+            int index = q[cur];
+            if(v[index] < v[j]) {
+                f[j] = max(f[j],f[index] + 1);
+                q[++tt] = j;
+                cur--;
+            }else {
+                break;
+            }
+
         }
         for (int i = 1; i < j ; ++i) {
             //根据必要条件，只去算符合严格上升的限制的情况

cf54c5baf988d19dff6a3c0c15e3a6f1c01ebeae
DA-MO-WANG <guohan741@163.com>
2022-07-19 10:18:00 +0800

    autosave leet

--- cpp/acwing/junior/zuichangshangshengzixulie.cpp
+++ cpp/acwing/junior/zuichangshangshengzixulie.cpp
@@ -8,7 +8,7 @@
 #include <cmath>
 
 using namespace std;
-int const int N = 1010;
+const int N = 1010;
 int n;
 int v[N];
 //dp结构的容量取决于状态的数目
@@ -19,13 +19,17 @@ int main() {
     cin >> n;
     for (int i = 1; i <= n; ++i) {
         cin >> v[i];
+        //cout << v[i] << " ";
     }
+
     //第一层循环遍历的是结尾的情况
     for (int j = 1; j <= n; ++j) {
         //第二层循环遍历的是倒数第二个位置的情况
-        for (int i = 0; i < j ; ++i) {
-            if(!i) f[j] = 1;
-            f[j] = max(f[j],f[i] + 1);
+        //每换一种集合，集合中的第一个初始情况都是这个序列第二位置没有，只有本身一个数的情况，这是下面情况的出发点
+        f[j] = 1;
+        for (int i = 1; i < j ; ++i) {
+            //根据必要条件，只去算符合严格上升的限制的情况
+            if(v[i] < v[j]) f[j] = max(f[j],f[i] + 1);
         }
     }
     int res = 0;

7cb02d299d2ca773780227b32b79db98f4a048a0
DA-MO-WANG <guohan741@163.com>
2022-07-22 11:07:00 +0800

    autosave leet

--- cpp/acwing/junior/zhengshuhuafen.cpp
+++ cpp/acwing/junior/zhengshuhuafen.cpp
@@ -7,16 +7,16 @@
 #include <algorithm>
 
 using namespace std;
-const int N = 1010;
+const int N = 1010 mod = 1e9 + 7;
 int n;
 int f[N][N];
 int main() {
     cin >> n;
-    f[1][1] = 1;
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 0; i <= n; ++i) f[i][0] = 1;
+    for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
             for (int k = 0; k * i <= j; ++k) {
-                f[i][j] += f[i - 1][j - k * i];
+                f[i][j] = (f[i][j] + f[i - 1][j - k * i]) % mod;
             }
         }
     }

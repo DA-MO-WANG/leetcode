e0ee023d8715b624090c1770eab209de81f69394
DA-MO-WANG <guohan741@163.com>
2022-07-22 09:22:00 +0800

    autosave leet

--- cpp/acwing/junior/zhengshuhuafen.cpp
+++ cpp/acwing/junior/zhengshuhuafen.cpp
@@ -7,8 +7,17 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+int n;
+int f[N];
 int main() {
-
+    cin >> n;
+    f[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int k = 1; k <= i; ++k) {
+            f[i] += f[i - k];
+        }
+    }
+    cout << f[n];
     return 0;
 }

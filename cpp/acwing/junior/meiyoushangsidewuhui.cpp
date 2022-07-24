58d5068a974655064a71834cd7b39e2cf3cd14e1
DA-MO-WANG <guohan741@163.com>
2022-07-24 13:45:00 +0800

    autosave leet

--- cpp/acwing/junior/meiyoushangsidewuhui.cpp
+++ cpp/acwing/junior/meiyoushangsidewuhui.cpp
@@ -8,8 +8,40 @@
 
 using namespace std;
 const int N = 6010;
-//临接表来表示表结构
-int main() {
+int n;
+//临接表来表示树结构
+int h[N], e[N], ne[N], idx;
+int happy[N];
+//状态表示
+int f[N][2];
+//找到根节点，职级最大的点-没有父节点的点
+bool is_father[N];
+//k -> l
+void add(int k, int l) {
+    e[idx] = l;
+    ne[idx] = h[k];
+    h[k] = idx;
+    is_father[k] = true;
+    idx++;
+}
 
+void dp(int n) {
+    
+}
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> happy[i];
+    }
+    while (--n) {
+        int l, k;
+        cin >> l >> k;
+        add(k,l);
+    }
+    for (int i = 0; i < ; ++i) {
+        
+    }
+    dp(n);
+    cout << max(f[n][0],f[n][1]);
     return 0;
 }

a0bf90fa4e39c33f72aa33cb42d34d5217f22b1b
DA-MO-WANG <guohan741@163.com>
2022-07-27 10:45:00 +0800

    autosave leet

--- cpp/acwing/junior/zuidabuxiangjiaoqujianshuliang.cpp
+++ cpp/acwing/junior/zuidabuxiangjiaoqujianshuliang.cpp
@@ -5,7 +5,10 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+//最大不相交的区间的数量
+//只有选择前后区间时，在不破坏相交的条件下，距离越近越好，也就是右端点越小越好
+//而排序后的相邻特性，满足这个要求
+//这种策略局部距离最近==》全局个数最多
 using namespace std;
 const int N = 1e5 + 10;
 struct Range{
@@ -21,6 +24,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         int l, r;
         cin >> l >> r;
+        //结构体还可以这样赋值
         range[i] = {l,r};
     }
     sort(range,range + n);

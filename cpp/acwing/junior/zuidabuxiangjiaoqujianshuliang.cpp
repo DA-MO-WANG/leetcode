23af8addb3443f8c3e50df439f1b36ec1e254cf3
DA-MO-WANG <guohan741@163.com>
2022-07-27 10:42:00 +0800

    autosave leet

--- cpp/acwing/junior/zuidabuxiangjiaoqujianshuliang.cpp
+++ cpp/acwing/junior/zuidabuxiangjiaoqujianshuliang.cpp
@@ -7,8 +7,30 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1e5 + 10;
+struct Range{
+    int l;
+    int r;
+    bool operator< (const Range &w)const {
+        return r < w.r;
+    }
+}range[N];
+int n;
 int main() {
-
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int l, r;
+        cin >> l >> r;
+        range[i] = {l,r};
+    }
+    sort(range,range + n);
+    int res = 0, ed = -2e9;
+    for (int i = 0; i < n; ++i) {
+        if(ed < range[i].l) {
+            res++;
+            ed = range[i].r;
+        }
+    }
+    cout << res << endl;
     return 0;
 }

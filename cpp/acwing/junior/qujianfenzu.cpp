5547aa4abc6f603e1b397cfeb47a85ea0ef453ff
DA-MO-WANG <guohan741@163.com>
2022-07-27 12:58:00 +0800

    autosave leet

--- cpp/acwing/junior/qujianfenzu.cpp
+++ cpp/acwing/junior/qujianfenzu.cpp
@@ -25,14 +25,16 @@ int main() {
         range[i] = {l,r};
     }
     sort(range,range + n);
-    //小根堆维护了所有组的最右右端点情况
+    //小根堆能够实时维护所有组的最右右端点情况
     //还记录了组的数量，每个组在结构里只有一个元素
     priority_queue<int, vector<int>, greater<int>> heap;
     for (int i = 0; i < n; ++i) {
         auto t = range[i];
         //有交集情况-新开组
-        if(heap.empty() && heap.top() >= range[i].l) heap.push(t.r)
+        //这里当第一次分组时，直接把第一个区间当第一组，也就是小根堆直接记录第一个区间右端点
+        if(heap.empty() ||  heap.top() >= range[i].l) heap.push(t.r);
         else {
+            //把当前区间加入到最小值所在组中
             heap.pop();
             heap.push(t.r);
         }

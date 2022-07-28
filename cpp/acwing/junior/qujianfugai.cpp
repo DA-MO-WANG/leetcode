1c7771284827789f6ddbc1e832a67298e578a019
DA-MO-WANG <guohan741@163.com>
2022-07-29 00:34:00 +0800

    autosave leet

--- cpp/acwing/junior/qujianfugai.cpp
+++ cpp/acwing/junior/qujianfugai.cpp
@@ -25,8 +25,19 @@ int main() {
         cin >> l >> r;
         range[i] = {l, r};
     }
-    for (int i = 0; i < n; ++i) {
-        int j 
+    sort(range,range + n);
+    int res = 0;
+    int start = s;
+    for (int i = 0; i < n; i++) {
+        int j = i;
+        //覆盖这个点：range[i].l < start
+        while(range[i].l < start) {
+            //找到覆盖这个点的区间右端点最大的区间
+            j = max(j,range[i].r);
+        }
+        start = j;
+        res++;
+        if(start >= t) break;
     }
     return 0;
 }

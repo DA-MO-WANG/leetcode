61abee45000cbd5a68887876a56ad0956d4cb841
DA-MO-WANG <guohan741@163.com>
2022-07-28 23:07:00 +0800

    autosave leet

--- cpp/acwing/junior/qujianfugai.cpp
+++ cpp/acwing/junior/qujianfugai.cpp
@@ -7,8 +7,26 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1e5 + 10;
+struct Range{
+    int l;
+    int r;
+    bool operator< (const Range &w)const  {
+        return l < w.l;
+    }
+}range[N];
+int s, t;
+int n;
 int main() {
-
+    cin >> s >> t;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int l, r;
+        cin >> l >> r;
+        range[i] = {l, r};
+    }
+    for (int i = 0; i < n; ++i) {
+        int j 
+    }
     return 0;
 }

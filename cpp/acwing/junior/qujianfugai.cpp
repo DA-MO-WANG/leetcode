bac1264ca4edaf2e56d19d14c31b3d9dbe1550fa
DA-MO-WANG <guohan741@163.com>
2022-07-29 09:21:00 +0800

    autosave leet

--- cpp/acwing/junior/qujianfugai.cpp
+++ cpp/acwing/junior/qujianfugai.cpp
@@ -15,10 +15,11 @@ struct Range{
         return l < w.l;
     }
 }range[N];
-int s, t;
+int start, t;
 int n;
+bool succcess;
 int main() {
-    cin >> s >> t;
+    cin >> start >> t;
     cin >> n;
     for (int i = 0; i < n; ++i) {
         int l, r;
@@ -27,17 +28,34 @@ int main() {
     }
     sort(range,range + n);
     int res = 0;
-    int start = s;
     for (int i = 0; i < n; i++) {
-        int j = i;
+        //技巧点：双指针如何实现
+        //双指针的作用就是找到在覆盖这些点的区间中找到一个右端点最大的那个区间
+        //双指针适用双重递进条件的情况
+
+        //从外层i循环的情况出发，不破坏外层循环
+        //需要设计一个临时的存储右端点的变量，每个start都要重新初始化这个变量，这个变量的初始值不影响max结果
+        int j = i , r = -2e9;
         //覆盖这个点：range[i].l < start
-        while(range[i].l < start) {
+        while(j < n && range[j].l <= start) {
             //找到覆盖这个点的区间右端点最大的区间
-            j = max(j,range[i].r);
+            r = max(r,range[j].r);
+            //循环覆盖start这个点的多个区间
+            j++;
         }
-        start = j;
-        res++;
-        if(start >= t) break;
+        //区间够不着start
+        if(r < start) break;
+        if(r > t) {
+            res++;
+            succcess = true;
+            break;
+        }
+        //更新start-这个选中覆盖的区间的右端点
+        start = r;
+        //下次覆盖从当前选中区间的下一个区间开始
+        //j ++ -1 + 1
+        i = j - 1;
     }
+    cout << res << endl;
     return 0;
 }

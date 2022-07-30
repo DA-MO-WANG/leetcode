3fb9700c20380948df5a0f09be1127966696ffad
DA-MO-WANG <guohan741@163.com>
2022-07-30 11:01:00 +0800

    autosave leet

--- cpp/acwing/junior/paiduidashui.cpp
+++ cpp/acwing/junior/paiduidashui.cpp
@@ -7,8 +7,19 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1e5 + 10;
+int n;
+int t[N];
 int main() {
-
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> t[i];
+    }
+    sort(t + 1,t + n + 1);
+    int res = 0;
+    for (int i = 1; i <= n; ++i) {
+        res += t[i] * (n - 1);
+    }
+    cout << res << endl;
     return 0;
 }

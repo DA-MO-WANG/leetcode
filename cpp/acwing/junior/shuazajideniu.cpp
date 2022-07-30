e17098d9f687e1a342d706df2158e45ac8d6f9ba
DA-MO-WANG <guohan741@163.com>
2022-07-30 15:47:00 +0800

    autosave leet

--- cpp/acwing/junior/shuazajideniu.cpp
+++ cpp/acwing/junior/shuazajideniu.cpp
@@ -7,8 +7,22 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 50010;
+typedef pair<int,int> PII;
+int n;
+PII ws[N];
 int main() {
-
+    cin >> n;
+    for (int i = 0; i < ; ++i) {
+        int w, int s;
+        cin >> w >> s;
+        ws[i] = {w + s,s};
+    }
+    sort(ws,ws + n);
+    int res = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        res += ws[i].first - ws[i].second;
+    }
+    cout << res - ws[n - 1].second << endl;
     return 0;
 }

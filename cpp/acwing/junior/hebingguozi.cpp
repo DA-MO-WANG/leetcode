cca0b26e7cab77258e38947a169cc0d330064038
DA-MO-WANG <guohan741@163.com>
2022-07-30 10:21:01 +0800

    autosave leet

--- cpp/acwing/junior/hebingguozi.cpp
+++ cpp/acwing/junior/hebingguozi.cpp
@@ -16,5 +16,13 @@ int main() {
         cin >> x;
         heap.push(x);
     }
+    int res = 0;
+    while (heap.size() > 1) {
+        int a = heap.top(), heap.pop();
+        int b = heap.top(), heap.pop();
+        res += a + b;
+        heap.push(res);
+    }
+    cout << res << endl;
     return 0;
 }

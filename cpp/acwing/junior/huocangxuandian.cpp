21d7ac7f2ed5bc5b310f1f76467d93a3555549a2
DA-MO-WANG <guohan741@163.com>
2022-07-30 11:37:00 +0800

    autosave leet

--- cpp/acwing/junior/huocangxuandian.cpp
+++ cpp/acwing/junior/huocangxuandian.cpp
@@ -5,8 +5,10 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
+typedef long long ll;
 const int N = 1e5 + 10;
 int n;
 int a[N];
@@ -15,5 +17,11 @@ int main() {
     for (int i = 1; i <= n; ++i) {
         cin >> a[i];
     }
+    sort(a + 1, a + n + 1);
+    ll res = 0;
+    for (int i = 1; i <= n; ++i) {
+        res += abs(a[i] - a[n / 2]);
+    }
+    cout << res << endl;
     return 0;
 }

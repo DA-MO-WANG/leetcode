843686efbe154ddd4303bdd8c346c91a164b4765
DA-MO-WANG <guohan741@163.com>
2022-07-30 16:10:01 +0800

    autosave leet

--- cpp/acwing/junior/shuazajideniu.cpp
+++ cpp/acwing/junior/shuazajideniu.cpp
@@ -10,19 +10,25 @@ using namespace std;
 const int N = 50010;
 typedef pair<int,int> PII;
 int n;
-PII ws[N];
+PII p[N];
 int main() {
     cin >> n;
     for (int i = 0; i < ; ++i) {
         int w, int s;
         cin >> w >> s;
-        ws[i] = {w + s,s};
+        p[i] = {w + s,s};
     }
     sort(ws,ws + n);
-    int res = 0;
-    for (int i = 0; i < n - 1; ++i) {
-        res += ws[i].first - ws[i].second;
+    //第一个值的风险值为0 - s1
+    int res = -2e9, sum = 0;
+    for (int i = 0; i < n; ++i) {
+        int s = p[i].second;
+        int w = p[i].first - s;
+        //最后记录风险值的最大值
+        res = max(res,sum - s);
+        //只要在相加之前，sum就记录的是上一个的和
+        sum += w;
     }
-    cout << res - ws[n - 1].second << endl;
+    cout << res << endl;
     return 0;
 }

88d26ce10d578ff8ff857c39ef227cdbeec67ee8
DA-MO-WANG <guohan741@163.com>
2022-07-30 11:34:00 +0800

    autosave leet

--- cpp/acwing/junior/huocangxuandian.cpp
+++ cpp/acwing/junior/huocangxuandian.cpp
@@ -7,8 +7,13 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1e5 + 10;
+int n;
+int a[N];
 int main() {
-
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
     return 0;
 }

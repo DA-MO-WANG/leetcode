fe2aabe0d415dbb0cbb080aa31bc0544e71fa6b0
DA-MO-WANG <guohan741@163.com>
2022-07-31 12:27:00 +0800

    autosave leet

--- cpp/acwing/junior/lianbiaoyouhuan.cpp
+++ cpp/acwing/junior/lianbiaoyouhuan.cpp
@@ -17,26 +17,28 @@ ListNode *entryNodeOfLoop(ListNode *head) {
     //证明环的思路1：只有存在环的情况下，前面的指针才有可能走后面的后面
     //思路2：一个指针走的位置以前是否走过，记忆化手段-map存储
     //思路3：快慢指针，两个速度不同的指针，如果没环的话，距离只会越拉越远；有环，存在相遇的可能
-//    ListNode dummy = new ListNode(-1);
-//    dummy.next = head;
-//    int left = head->next;
-//    int right = head;
-//    while(left != right) {
-//        if(left == NULL || right == NULL) return null;
-//        left = left.next.next;
-//        right = right.next;
-//    }
-    unorderd_map<ListNode,int> map;
-    ListNode cur = head;
-    int index = 1;
-    while(cur != NULL) {
-        if(!map[cur]) {
-            map.insert({cur,index++})
-        }else {
-            return cur;
-        }
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+    int left = head->next;
+    int right = head;
+    while(left != right) {
+        if(left == NULL || right == NULL) return null;
+        left = left.next.next;
+        right = right.next;
     }
-    return null;
+
+    //思路2需要给链表写一个哈希函数
+//    unorderd_map<ListNode,int> map;
+//    ListNode cur = head;
+//    int index = 1;
+//    while(cur != NULL) {
+//        if(!map[cur]) {
+//            map.insert({cur,index++})
+//        }else {
+//            return cur;
+//        }
+//    }
+//    return null;
 
 }
 int main() {

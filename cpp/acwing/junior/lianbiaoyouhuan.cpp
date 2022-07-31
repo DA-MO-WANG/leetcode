000d058fd9e32606e4e323859bcac11376c84905
DA-MO-WANG <guohan741@163.com>
2022-07-31 11:58:00 +0800

    autosave leet

--- cpp/acwing/junior/lianbiaoyouhuan.cpp
+++ cpp/acwing/junior/lianbiaoyouhuan.cpp
@@ -7,8 +7,15 @@
 #include <algorithm>
 
 using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 ListNode *entryNodeOfLoop(ListNode *head) {
-
+    //需要两个指针，一个指针在前面，一个指针在后面
+    //只有存在环的情况下，前面的指针才有可能走后面的后面
+    int 
 }
 int main() {
 

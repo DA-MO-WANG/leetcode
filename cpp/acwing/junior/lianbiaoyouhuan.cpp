1866f8d45949ec5a16af7a13b551ae692a18ffb9
DA-MO-WANG <guohan741@163.com>
2022-07-31 12:13:00 +0800

    autosave leet

--- cpp/acwing/junior/lianbiaoyouhuan.cpp
+++ cpp/acwing/junior/lianbiaoyouhuan.cpp
@@ -17,14 +17,19 @@ ListNode *entryNodeOfLoop(ListNode *head) {
     //证明环的思路1：只有存在环的情况下，前面的指针才有可能走后面的后面
     //思路2：一个指针走的位置以前是否走过，记忆化手段-map存储
     //思路3：快慢指针，两个速度不同的指针，如果没环的话，距离只会越拉越远；有环，存在相遇的可能
-    ListNode dummy = new ListNode(-1);
-    dummy.next = head;
-    int left = head->next;
-    int right = head;
-    while(left != right) {
-        if(left == NULL || right == NULL) return null;
-        left = left.next.next;
-        right = right.next;
+//    ListNode dummy = new ListNode(-1);
+//    dummy.next = head;
+//    int left = head->next;
+//    int right = head;
+//    while(left != right) {
+//        if(left == NULL || right == NULL) return null;
+//        left = left.next.next;
+//        right = right.next;
+//    }
+    unorderd_map<ListNode,int> map;
+    ListNode cur = head;
+    while(cur != NULL) {
+        if(map[cur])
     }
 
 }

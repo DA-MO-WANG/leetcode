5684f390d911bea24c98e4c2bcf5aba449747b0e
DA-MO-WANG <guohan741@163.com>
2022-07-31 11:16:00 +0800

    autosave leet

--- cpp/acwing/junior/shuangzhizhentihuankongge.cpp
+++ cpp/acwing/junior/shuangzhizhentihuankongge.cpp
@@ -7,13 +7,35 @@
 #include <algorithm>
 //要求：控制多余空间，争取O1
 using namespace std;
-
-int main() {
-    //先计算新字符串的新长度
-
+string replaceSpaces(string &str) {
+    int len = 0;
+    //先计算新字符串的新长度-->计算空格的数量
+    while(str[i]) {
+        if(str[i] == ' ') {
+            len += 3;
+        }else {
+            len++;
+        }
+    }
     //重新分配新长度大小的空间
+    str.resize(len);
 
 
     //从后往前遍历，填充，同时替换
+    int i = str.size() - 1;
+    int j = len - 1;
+    for (int i = str.size() - 1; i >= 0; i--) {
+        if(str[i] == ' ') {
+            str[j--] = '0';
+            str[j--] = '2';
+            str[j--] = '%';
+        }esle {
+            str[j--] = str[i];
+        };
+    }
+    return str;
+}
+int main() {
+
     return 0;
 }

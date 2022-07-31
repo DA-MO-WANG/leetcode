d1ec356fc465417916c17a5c578668450f7cac65
DA-MO-WANG <guohan741@163.com>
2022-07-31 11:10:00 +0800

    autosave leet

--- cpp/acwing/junior/shuangzhizhentihuankongge.cpp
+++ cpp/acwing/junior/shuangzhizhentihuankongge.cpp
@@ -5,10 +5,15 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+//要求：控制多余空间，争取O1
 using namespace std;
 
 int main() {
+    //先计算新字符串的新长度
 
+    //重新分配新长度大小的空间
+
+
+    //从后往前遍历，填充，同时替换
     return 0;
 }

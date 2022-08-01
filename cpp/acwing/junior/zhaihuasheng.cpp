ea285104a1380201fed0d969154921c9fc420193
DA-MO-WANG <guohan741@163.com>
2022-08-01 09:31:00 +0800

    autosave leet

--- cpp/acwing/junior/zhaihuasheng.cpp
+++ cpp/acwing/junior/zhaihuasheng.cpp
@@ -7,11 +7,13 @@
 #include <algorithm>
 #include <cmath>
 //思路：典型dp
-//
+//每一步，有两种走法，问什么样的走法，花生数量最多---带权路径和最大
+//每一步问题，拆解成可重复的结构：状态转移，从而不断降级，一致转移到dp基状态
+//状态的传递具有有限性，确定性，所以必然能通向最终结果
 using namespace std;
 const int N = 110;
 int t;
-int f[N][N], c[N][N];
+int f[N], c[N][N];
 int main() {
     cin >> t;
     while (t--) {
@@ -24,10 +26,10 @@ int main() {
         }
         for (int i = 1; i <= r; ++i) {
             for (int j = 1; j <= c; ++j) {
-                f[i][j] = max(f[i][j - 1],f[i - 1][j]) + c[i][j];
+                f[j] = max(f[j - 1],f[j]) + c[i][j];
             }
         }
-        cout << f[r][c] << endl;
+        cout << f[c] << endl;
     }
     return 0;
 }

bb1fc4c98b2457f3b33707051e4c04353cc9656d
DA-MO-WANG <guohan741@163.com>
2022-08-01 09:20:00 +0800

    autosave leet

--- cpp/acwing/junior/zhaihuasheng.cpp
+++ cpp/acwing/junior/zhaihuasheng.cpp
@@ -5,10 +5,28 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
-
+const int N = 110;
+int t;
+int f[N][N], c[N][N];
 int main() {
-
+    cin >> t;
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        for (int i = 1; i <= r; ++i) {
+            for (int j = 1; j <= c; ++j) {
+                cin >> c[i][j];
+            }
+        }
+        for (int i = 1; i <= r; ++i) {
+            for (int j = 1; j <= c; ++j) {
+                f[i][j] = max(f[i][j - 1],f[i - 1][j]) + c[i][j];
+            }
+        }
+        cout << f[r][c] << endl;
+    }
     return 0;
 }

803e82c2f7c152ed7411a075af7d3294a106b286
DA-MO-WANG <guohan741@163.com>
2022-08-01 11:05:00 +0800

    autosave leet

--- cpp/acwing/junior/zuiditongxingfeiyong.cpp
+++ cpp/acwing/junior/zuiditongxingfeiyong.cpp
@@ -5,12 +5,27 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 const int N = 110;
 int n;
 int f[N][N], e[N][N];
 int dx[4] = {-1,1,0,0}, dy[4] = {0,0,-1,1};
+int dfs(int x, int y, int count) {
+    if(!x || !y) {
+        return 0;
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        int row = x + dx[i], col = y + dy[i];
+        if(row && row <= n && col && col <= n && count <= 2 * n - 1) {
+
+            f[x][y] = min(dfs(row,col,++count),f[x][y]) + e[x][y];
+        }
+    }
+    return f[x][y];
+}
 int main() {
     cin >> n;
     for (int i = 1; i <= n; ++i) {
@@ -18,20 +33,8 @@ int main() {
             cin >> e[i][j];
         }
     }
-    memset(f,0, sizeof(f));
+    memset(f,0x3f, sizeof(f));
     int count = 0;
-    for (int x = 1; x <= n; ++x) {
-        for (int y = 1; y <= n; ++y) {
-            for (int i = 0; i < 4; ++i) {
-                int row = x + dx[i], col = y + dy[i];
-                if(row && row <= n && col && col <= n && count <= 2 * n - 1) {
-                    f[x][y] = min(f[row][col],f[x][y]) + e[x][y];
-                }
-            }
-            count ++;
-
-        }
-    }
     cout << f[n][n] << endl;
 
     return 0;

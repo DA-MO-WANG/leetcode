d404f135ebcf3dbb2fe1bbaee7717d68baf4be55
DA-MO-WANG <guohan741@163.com>
2022-08-01 10:47:00 +0800

    autosave leet

--- cpp/acwing/junior/zuiditongxingfeiyong.cpp
+++ cpp/acwing/junior/zuiditongxingfeiyong.cpp
@@ -11,12 +11,6 @@ const int N = 110;
 int n;
 int f[N][N], e[N][N];
 int dx[4] = {-1,1,0,0}, dy[4] = {0,0,-1,1};
-int dp(int x, int y) {
-    for (int i = 0; i < 4; ++i) {
-        int row = x + dx[i], col = y + dy[i];
-        f[x][y] =
-    }
-}
 int main() {
     cin >> n;
     for (int i = 1; i <= n; ++i) {

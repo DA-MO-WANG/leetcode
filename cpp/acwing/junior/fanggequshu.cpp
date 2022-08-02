5f1f3120410eb75d172286134d797e516f36de94
DA-MO-WANG <guohan741@163.com>
2022-08-02 18:41:00 +0800

    autosave leet

--- cpp/acwing/junior/fanggequshu.cpp
+++ cpp/acwing/junior/fanggequshu.cpp
@@ -20,14 +20,20 @@ int main() {
     while (cin >> row >> col >> v, row || col || v) {
         w[row][col] = v;
     }
-    for (int k = 1; k <= 2 * N; k++) {
-        for (int i = 0; i < ; ++i) {
-            
+
+    for (int k = 1; k < 2 * n; k++) {
+        for (int i1 = 1; i1 <= n; ++i1) {
+            for (int i2 = 0; i2 <= n; ++i2) {
+                int t = &f[k][i1][i2];
+                if(i1 != i2) t += w[i1][k - i1];
+                t = max(t,f[k - 1][i1 - 1][i2 - 1] + w[i2][k - i2]);
+                t = max(t,f[k - 1][i1 - 1][i2] + w[i2][k - i2]);
+                t = max(t,f[k - 1][i1][i2 - 1] + w[i2][k - i2]);
+                t = max(t,f[k - 1][i1][i2] + w[i2][k - i2]);
+            }
+
         }
     }
-    int res = f[n][n];
-    dp();
-    res += f[n][n];
-    cout << res << endl;
+    cout << f[2 * n - 1][n][n] << endl;
     return 0;
 }

026ff6b816dae4f9360737437cd43a9e3237b158
DA-MO-WANG <guohan741@163.com>
2022-08-02 19:20:00 +0800

    autosave leet

--- cpp/acwing/junior/fanggequshu.cpp
+++ cpp/acwing/junior/fanggequshu.cpp
@@ -10,7 +10,9 @@ using namespace std;
 const int N = 15;
 int n;
 //思路打通了，代码实现细节上还有很多值得推敲的地方
-
+//这里状态表示是f(k,i1,i2)
+//k不是指的走的步数，这里我理解错了——》k的变化的次数是受步数限制的
+//k实质指的是坐标和，坐标和从1+1 变成 n + n ,经历了2n - 1 步变化
 int f[N * 2][N][N], w[N][N];
 int main() {
     cin >> n;
@@ -20,8 +22,9 @@ int main() {
     while (cin >> row >> col >> v, row || col || v) {
         w[row][col] = v;
     }
-
+    //坐标和最开始是(1,1) ,也就是2，最大是(n,n) 也就是 2n
     for (int k = 2; k <= 2 * n; k++) {
+        //i1代表的是其中一个指针的横坐标，因为和是定下来了，所以纵坐标可以做为衍生变量
         for (int i1 = 1; i1 <= n; ++i1) {
             for (int i2 = 1; i2 <= n; ++i2) {
                 int j1 = k - i1, j2 = k - i2;

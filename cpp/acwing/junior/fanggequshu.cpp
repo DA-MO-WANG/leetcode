f9426ddbcbe32b57e4ebdcb596cc148432844346
DA-MO-WANG <guohan741@163.com>
2022-08-02 18:52:00 +0800

    autosave leet

--- cpp/acwing/junior/fanggequshu.cpp
+++ cpp/acwing/junior/fanggequshu.cpp
@@ -24,16 +24,20 @@ int main() {
     for (int k = 1; k < 2 * n; k++) {
         for (int i1 = 1; i1 <= n; ++i1) {
             for (int i2 = 0; i2 <= n; ++i2) {
-                int t = &f[k][i1][i2];
-                if(i1 != i2) t += w[i1][k - i1];
-                t = max(t,f[k - 1][i1 - 1][i2 - 1] + w[i2][k - i2]);
-                t = max(t,f[k - 1][i1 - 1][i2] + w[i2][k - i2]);
-                t = max(t,f[k - 1][i1][i2 - 1] + w[i2][k - i2]);
-                t = max(t,f[k - 1][i1][i2] + w[i2][k - i2]);
+                int j1 = k - i1, j2 = k - i2;
+                if(j1 >=1 && j1 <= n && j2 >= 1 && j2 <= n) {
+                    int t = &w[i1][j1];
+                    if(i1 != i2) t += w[i2][j2];
+                    t = max(t,f[k - 1][i1 - 1][i2 - 1] + t);
+                    t = max(t,f[k - 1][i1 - 1][i2] + t);
+                    t = max(t,f[k - 1][i1][i2 - 1] + t);
+                    t = max(t,f[k - 1][i1][i2] + t);
+                }
+
             }
 
         }
     }
-    cout << f[2 * n - 1][n][n] << endl;
+    cout << f[2 * n][n][n] << endl;
     return 0;
 }

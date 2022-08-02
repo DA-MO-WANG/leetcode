6121ffa7856f8d02c16f49f252acdf6a05df13d3
DA-MO-WANG <guohan741@163.com>
2022-08-02 16:17:01 +0800

    autosave leet

--- cpp/acwing/junior/fanggequshu.cpp
+++ cpp/acwing/junior/fanggequshu.cpp
@@ -7,8 +7,37 @@
 #include <algorithm>
 
 using namespace std;
-
+int n;
+int f[20][20], w[20][20];
+void dp() {
+    memeset(f, 0 , sizeof(f));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if(f[i - 1][j] > f[i][j - 1]) {
+                f[i][j] = f[i - 1][j] + w[i][j];
+                w[i - 1][j] = 0;
+            }else {
+                f[i][j] = f[i][j - 1] + w[i][j];
+                w[i][j - 1] = 0;
+            }
+        }
+    }
+}
 int main() {
+    cin >> n;
+    while (true) {
 
+        int row , col, v;
+        cin >> row >> col >> v;
+        if(!row || !col) {
+            break;
+        }
+        w[row][col] = v;
+    }
+    dp();
+    int res = f[n][n];
+    dp();
+    res += f[n][n];
+    cout << res << endl;
     return 0;
 }

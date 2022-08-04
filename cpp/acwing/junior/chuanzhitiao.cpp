243bbc0513f914b30f6d9669785a90c32920366d
DA-MO-WANG <guohan741@163.com>
2022-08-04 18:46:00 +0800

    autosave leet

--- cpp/acwing/junior/chuanzhitiao.cpp
+++ cpp/acwing/junior/chuanzhitiao.cpp
@@ -23,19 +23,20 @@ int main() {
     }
 
     //坐标和最开始是(1,1) ,也就是2，最大是(n,n) 也就是 2n
-    for (int k = 1; k <= m + n - 1; k++) {
+    for (int k = 2; k <= m + n - 1; k++) {
 
         for (int i1 = 1; i1 <= m; ++i1) {
             //i2 做为第二个指针
-            for (int i2 = m; i2 >= 1; i2--) {
-                int j1 = k + 2 - i1, j2 = m + n - k - i2;
-                if(j1 >=1 && j1 <= n && j2 >= 1 && j2 <= n && (i1 + j1) * 2 != (m + n + 2)) {
-                    int t = w[i1][j1] + w[i2][j2];
+            for (int i2 = 1; i2 <= m; i2++) {
+                int j1 = k + 2 - i1, j2 = k + 2 - i2;
+                if(j1 >=1 && j1 <= n && j2 >= 1 && j2 <= n) {
+                    int t = w[i1][j1];
+                    if(i1 != i2 && j1 != j2) t += w[i2][j2];
                     int &res = f[k][i1][i2];
                     //这里是四个依赖状态的max运算
-                    res = max(res,f[k - 1][i1 - 1][i2 + 1] + t);
+                    res = max(res,f[k - 1][i1 - 1][i2 - 1] + t);
                     res = max(res,f[k - 1][i1 - 1][i2] + t);
-                    res = max(res,f[k - 1][i1][i2 + 1] + t);
+                    res = max(res,f[k - 1][i1][i2 - 1] + t);
                     res = max(res,f[k - 1][i1][i2] + t);
                 }
 
@@ -43,6 +44,6 @@ int main() {
 
         }
     }
-    cout << f[m + n - 1][n][n] << endl;
+    cout << f[m + n - 1][m][m] << endl;
     return 0;
 }

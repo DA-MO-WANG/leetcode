eedf71ea59e433ccb0cec34ef973466276bd904f
DA-MO-WANG <guohan741@163.com>
2022-08-05 10:01:01 +0800

    autosave leet

--- cpp/acwing/junior/guaidaojidehuaxiangyi.cpp
+++ cpp/acwing/junior/guaidaojidehuaxiangyi.cpp
@@ -7,8 +7,44 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 110;
+int k, n;
+int v[N], f[2][N];
 int main() {
+    cin >> k;
+    while(k--) {
+        cin >> n;
+        for (int i = 1; i <= n; ++i) {
+            cin >> v[i];
+        }
 
+        for (int i = 1; i <= n; ++i) {
+            if(i == 1) {
+                f[0][i] = 1;
+                continue;
+            }
+            for (int j = 1; j < i; ++j) {
+                if(v[j] < v[i]) f[0][i] = max(f[0][i],f[0][j] + 1);
+            }
+        }
+
+        for (int i = n; i >= 1; i--) {
+            if(i == n) {
+                f[1][i] = 1;
+                continue;
+            }
+            for (int j = n; j > i; j--) {
+                if(v[j] < v[i]) f[1][i] = max(f[1][i],f[1][j] + 1);
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                res = max(res, f[i][j]);
+            }
+        }
+        cout << res <<endl;
+
+    }
     return 0;
 }

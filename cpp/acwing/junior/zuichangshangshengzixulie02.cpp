05b5a7442c7a8a1253b6ce285d9d23d319d40815
DA-MO-WANG <guohan741@163.com>
2022-08-05 17:06:00 +0800

    autosave leet

--- cpp/acwing/junior/zuichangshangshengzixulie02.cpp
+++ cpp/acwing/junior/zuichangshangshengzixulie02.cpp
@@ -7,8 +7,24 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+int v[N],f[N];
+int n;
 int main() {
-
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+        f[i] = 1;
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < i; ++k) {
+            if(v[k] < v[i]) f[i] = max(f[i],f[j] + 1);
+        }
+    }
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        res = max(f[i],res);
+    }
+    cout << res << endl;
     return 0;
 }

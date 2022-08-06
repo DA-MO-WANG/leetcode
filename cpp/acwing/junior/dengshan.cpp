14b9d3ebeb32ab1bf52bd0f6dbfa1f37568e933c
DA-MO-WANG <guohan741@163.com>
2022-08-06 17:00:01 +0800

    autosave leet

--- cpp/acwing/junior/dengshan.cpp
+++ cpp/acwing/junior/dengshan.cpp
@@ -1,5 +1,5 @@
 //
-// Created by rich heart on 06/08/2022.
+// Created by rich heart on 05/08/2022.
 //
 
 #include <cstring>
@@ -7,8 +7,33 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+int v[N],f[N],g[N];
+int n;
 int main() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> v[i];
+    }
+    for (int i = 1; i <= n; ++i) {
+        f[i] = 1;
+        for (int k = 1; k < i; ++k) {
+            if(v[k] < v[i]) f[i] = max(f[i],f[k] + 1);
 
+        }
+    }
+    for (int i = n ; i; i--) {
+        g[i] = 1;
+        for (int k = n; k > i; k--) {
+            if(v[k] < v[i]) g[i] = max(g[i],g[k] + 1);
+
+        }
+    }
+    int res = 0;
+    //枚举拐点
+    for (int i = 1; i <= n; ++i) {
+        res = max(f[i] + g[i] - 1,res);
+    }
+    cout << n - res << endl;
     return 0;
 }

c94b74396b7375c9005a4b94f2a40aac2b9198cf
DA-MO-WANG <guohan741@163.com>
2022-08-07 11:03:00 +0800

    autosave leet

---
+++ cpp/acwing/junior/lanjiedaodan.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rich heart on 07/08/2022.
+//
+
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int N = 
+int main() {
+
+    return 0;
+}

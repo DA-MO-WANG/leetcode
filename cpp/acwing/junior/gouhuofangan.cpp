cc2964ced9e104f68afd067a1468a6c3834a15df
DA-MO-WANG <guohan741@163.com>
2022-08-08 12:16:00 +0800

    autosave leet

--- cpp/acwing/junior/gouhuofangan.cpp
+++ cpp/acwing/junior/gouhuofangan.cpp
@@ -5,10 +5,25 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
+int weight[3] = {2,3,5};
+vector<int> path;
+void dfs(int price, int num) {
+    if(!price) {
+        cout << path;
+        return;
+    }
+    for (int i = 0; i < 3; ++i) {
+        if(price && num) path.pushweight[i];
+        dfs(price - weight[i],--num);
+        path
 
+    }
+}
+int m,n;
 int main() {
-
+    cin >> m >> n;
+    dfs(m,n);
     return 0;
 }

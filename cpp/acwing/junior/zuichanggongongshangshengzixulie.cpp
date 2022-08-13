78132c9b3214dd979f10eff0c1988af80dbd20f4
DA-MO-WANG <guohan741@163.com>
2022-08-13 09:47:00 +0800

    autosave leet

--- cpp/acwing/junior/zuichanggongongshangshengzixulie.cpp
+++ cpp/acwing/junior/zuichanggongongshangshengzixulie.cpp
@@ -20,17 +20,32 @@ int main() {
     for (int j = 1; j <= n; ++j) {
         cin >> b[j];
     }
+//    for (int i = 1; i <= n; ++i) {
+//        for (int j = 1; j <= n; ++j) {
+//            //a[i]一定不在公共上升子序列中
+//            f[i][j] = f[i - 1][j];
+//            //a[i]一定在公共上升子序列中
+//            if(a[i] == b[j]) {
+//                //第二层的划分：最初始是倒数第二个元素为空，此时公共上升子序列为1
+//                f[i][j] = max(f[i][j],1);
+//                for (int k = 1; k < j; ++k) {
+//                    if(b[k] < b[j]) f[i][j] = max(f[i][j],f[i][k] + 1);
+//                }
+//            }
+//        }
+//    }
+
+    //优化版本一：剪掉一维
     for (int i = 1; i <= n; ++i) {
+        //循环求前缀最大值
+        int maxv = 0;
         for (int j = 1; j <= n; ++j) {
-            //a[i]一定不在公共上升子序列中
-            f[i][j] = f[i - 1][j];
-            //a[i]一定在公共上升子序列中
-            if(a[i] == b[j]) {
-                f[i][j] = max(f[i][j],1);
-                for (int k = 1; k < j; ++k) {
-                    if(b[k] < b[j]) f[i][j] = max(f[i][j],f[i][k] + 1);
-                }
-            }
+            //复用第二层循环，当i不变时，其实把j从1循环到后面
+            //在这个判断中，maxv = f[i][j - 1]
+            if(a[i] == b[j]) f[i][j] = max(maxv + 1,f[i][j]);
+
+            //a[i]一定不会在公共上升子序列中
+            if(b[j] < a[i]) maxv = max(f[i - 1][j],maxv);
         }
     }
     int res = 0;

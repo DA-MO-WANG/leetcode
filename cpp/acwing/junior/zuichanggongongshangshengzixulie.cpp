856bd560436af6436a760092f8e42eaeea225a75
DA-MO-WANG <guohan741@163.com>
2022-08-13 09:33:01 +0800

    autosave leet

--- cpp/acwing/junior/zuichanggongongshangshengzixulie.cpp
+++ cpp/acwing/junior/zuichanggongongshangshengzixulie.cpp
@@ -10,6 +10,7 @@ using namespace std;
 const int N = 3010;
 int n;
 int a[N],b[N];
+//在前i个元素中出现，又在前j个元素中出现，同时以b[j]结尾的公共上升子序列
 int f[N][N];
 int main() {
     cin >> n;
@@ -21,9 +22,11 @@ int main() {
     }
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
+            //a[i]一定不在公共上升子序列中
             f[i][j] = f[i - 1][j];
+            //a[i]一定在公共上升子序列中
             if(a[i] == b[j]) {
-                f[i][j] = max(f[i][j],f[i][1]);
+                f[i][j] = max(f[i][j],1);
                 for (int k = 1; k < j; ++k) {
                     if(b[k] < b[j]) f[i][j] = max(f[i][j],f[i][k] + 1);
                 }

2b67ba2bd26716468b996a8894659c5c60a9b793
DA-MO-WANG <guohan741@163.com>
2022-08-16 10:29:00 +0800

    autosave leet

--- cpp/acwing/junior/caiyao.cpp
+++ cpp/acwing/junior/caiyao.cpp
@@ -12,7 +12,7 @@ const int T = 1010, M = 110;
 //w用来存储每个物品的价值情况
 int t[M],w[M];
 //f(i,j) 代表前i个物品中，不超过j时间内的所有选法的情况
-int f[M];
+int f[T];
 int main() {
     int times,nums;
     cin >> times >> nums;
@@ -22,13 +22,12 @@ int main() {
         t[i] = x, w[i] = y;
     }
     for (int i = 1; i <= nums; ++i) {
-        for (int j = t[i]; j <= times; ++j) {
+        for (int j = times; j >= t[i]; ++j) {
             //集合划分：第i个物品选法中一定不存在
             //第i个物品选法中一定存在
-            f[i] = f[i - 1];
-            if(j - t[i] >= 0) f[i] = max(f[i],f[i - 1] + w[i]);
+            f[j] = max(f[j],f[j - t[i]] + w[i]);
         }
     }
-    cout << f[nums] << endl;
+    cout << f[times] << endl;
     return 0;
 }

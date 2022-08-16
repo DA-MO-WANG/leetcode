d2a366ae1fcfb8278774730ddd3c133b26fbaaf2
DA-MO-WANG <guohan741@163.com>
2022-08-16 08:34:00 +0800

    autosave leet

--- cpp/acwing/junior/caiyao.cpp
+++ cpp/acwing/junior/caiyao.cpp
@@ -5,10 +5,22 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-
+#include <cmath>
 using namespace std;
-
+const int T = 1010, M = 110;
+int t[M],w[M];
 int main() {
-
+    int times,nums;
+    cin >> times >> nums;
+    for (int i = 1; i <= nums; ++i) {
+        int x, y;
+        t[i] = x, w[i] = y;
+    }
+    for (int i = 1; i <= times; ++i) {
+        for (int j = 1; j <= nums; ++j) {
+            if(j - w[i] > 0) f[i][j] = max(f[i - 1][j],f[i - 1][j - w[i]] + w[i]);
+        }
+    }
+    cout << f[times][nums] << endl;
     return 0;
 }

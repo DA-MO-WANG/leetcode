cca5940c08e771e45a6a80ab1429e3389e32414f
DA-MO-WANG <guohan741@163.com>
2022-09-03 16:17:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao3.cpp
+++ cpp/acwing/junior/duochongbeibao3.cpp
@@ -20,9 +20,10 @@ int main() {
                 if(hh <= tt && q[hh] < k - s * v) hh++;
                 if(hh <= tt) f[k] = max(f[k],g[q[hh]] + (k - q[hh]) / v * w)
                 while(hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w) tt--;
-                
+                q[++tt] = k;
             }
         }
     }
+    cout << f[m] << endl;
     return 0;
 }

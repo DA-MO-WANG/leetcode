266383c652a2c35635ad0399b6a65d0a4ea08403
DA-MO-WANG <guohan741@163.com>
2022-09-03 16:09:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao3.cpp
+++ cpp/acwing/junior/duochongbeibao3.cpp
@@ -12,7 +12,14 @@ int main() {
     cin >> n >> m;
     for (int i = 0; i < n; ++i) {
         int v, w, s;
-        cin >>  
+        cin >> v >> w >> s;
+        memcpy(g, f, sizeof(f));
+        for (int j = 0; j < v; ++j) {
+            int hh = 0, tt = -1;
+            for (int k = j; k <= m; ++k) {
+                if()
+            }
+        }
     }
     return 0;
 }

638626d25dab36dd7c5fc52fd749826b89c2d813
DA-MO-WANG <guohan741@163.com>
2022-09-03 16:20:00 +0800

    autosave leet

--- cpp/acwing/junior/duochongbeibao3.cpp
+++ cpp/acwing/junior/duochongbeibao3.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 const int N = 20010;
 int f[N],g[N],q[N];
+int n, m;
 int main() {
     cin >> n >> m;
     for (int i = 0; i < n; ++i) {
@@ -17,9 +18,9 @@ int main() {
         memcpy(g, f, sizeof(f));
         for (int j = 0; j < v; ++j) {
             int hh = 0, tt = -1;
-            for (int k = j; k <= m; ++k) {
+            for (int k = j; k <= m; k += v) {
                 if(hh <= tt && q[hh] < k - s * v) hh++;
-                if(hh <= tt) f[k] = max(f[k],g[q[hh]] + (k - q[hh]) / v * w)
+                if(hh <= tt) f[k] = max(f[k],g[q[hh]] + (k - q[hh]) / v * w);
                 while(hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w) tt--;
                 q[++tt] = k;
             }

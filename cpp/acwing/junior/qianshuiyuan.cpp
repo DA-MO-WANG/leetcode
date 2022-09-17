bbcf18b923e1fa2d0df0fabd155b7e618f3aded4
DA-MO-WANG <zard20201116@gmail.com>
2022-09-17 14:02:00 +0800

    autosave leet

--- cpp/acwing/junior/qianshuiyuan.cpp
+++ cpp/acwing/junior/qianshuiyuan.cpp
@@ -8,22 +8,20 @@
 
 using namespace std;
 const int N = 110, k = 1010;
-int f[k][N][N];
+int f[N][N];
 int o[k],d[k],w[k];
 int main() {
     int m, n, k;
     cin >> m >> n >> k;
+    f[0][0] = 0;
     for (int i = 1; i <= k; ++i) {
         cin >> o[i] >> d[i] >> w[i];
-        for (int j = 0; j <= m; ++j) {
-            for (int l = 0; l <= n; ++l) {
-                f[i][j][k] = f[i - 1][j][k];
-                if(j >= o[i] && l >= d[i]) {
-                    f[i][j][k] = min(f[i][j][k],f[i - 1][j - o[i]][l - d[i]] + w[i]);
-                }
+        for (int j = m; j >= 0; j--) {
+            for (int l = n; l >= 0; l--) {
+                f[j][k] = min(f[j][k],f[max(0,j - o[i])][max(,l - d[i])] + w[i]);
             }
         }
     }
-    cout << f[k][m][n];
+    cout << f[m][n];
     return 0;
 }

363d7ddd35da30826b0585255e43586090742615
DA-MO-WANG <zard20201116@gmail.com>
2022-09-17 19:48:01 +0800

    autosave leet

--- cpp/acwing/junior/qianshuiyuan.cpp
+++ cpp/acwing/junior/qianshuiyuan.cpp
@@ -14,11 +14,12 @@ int main() {
     int m, n, k;
     cin >> m >> n >> k;
     f[0][0] = 0;
+    memset(f,0x3f, sizeof(f));
     for (int i = 1; i <= k; ++i) {
         cin >> o[i] >> d[i] >> w[i];
         for (int j = m; j >= 0; j--) {
             for (int l = n; l >= 0; l--) {
-                f[j][k] = min(f[j][k],f[max(0,j - o[i])][max(,l - d[i])] + w[i]);
+                f[j][l] = min(f[j][l],f[max(0,j - o[i])][max(0,l - d[i])] + w[i]);
             }
         }
     }

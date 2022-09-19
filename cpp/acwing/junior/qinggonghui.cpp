ca2420060f8bc992e35187dc1d806cd2af2f0967
DA-MO-WANG <zard20201116@gmail.com>
2022-09-19 22:30:00 +0800

    autosave leet

--- cpp/acwing/junior/qinggonghui.cpp
+++ cpp/acwing/junior/qinggonghui.cpp
@@ -16,7 +16,7 @@ int main() {
         int v, w ,s;
         cin >> v >> w >> s;
         for (int j = m; j >= v ; j--) {
-            for (int k = 0; k <= s; ++k) {
+            for (int k = 0; k <= s && k * v <= j; ++k) {
                 f[j] = max(f[j],f[j - k * v] + k * w);
             }
         }

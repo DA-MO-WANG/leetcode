cf4932b6bc94c9d90b1c8fb7e429f88cd73678ab
DA-MO-WANG <zard20201116@gmail.com>
2022-09-24 23:08:01 +0800

    autosave leet

--- cpp/acwing/junior/maishu.cpp
+++ cpp/acwing/junior/maishu.cpp
@@ -7,8 +7,16 @@
 #include <algorithm>
 
 using namespace std;
-
+const int N = 1010;
+int f[N],n;
+int v[4] = {10,20,50,100};
 int main() {
-
+    cin >> n;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = v[i]; j <= n; ++j) {
+            f[j] += f[j - v[i]];
+        }
+    }
+    cout << f[n];
     return 0;
 }

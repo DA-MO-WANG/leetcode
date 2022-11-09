f8470a81e3c95b5c6e241629715dc5924078e83e
DA-MO-WANG <zard20201116@gmail.com>
2022-11-09 10:49:00 +0800

    autosave leet

--- cpp/acwing/junior/mergesort01.cpp
+++ cpp/acwing/junior/mergesort01.cpp
@@ -7,8 +7,24 @@
 #include <algorithm>
 
 using namespace std;
+const int N = 1e5 + 10;
+int q[N], n;
+
+void merge(int l, int mid, int r) {
+    
+}
+void merge_sort(int l, int r) {
+    int mid = l + r >> 1;
+    merge_sort(l, mid);
+    merge_sort(mid + 1, r);
+    merge(l,mid,r);
+}
 
 int main() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> q[i];
+    }
 
     return 0;
 }

ff960e03af2a3d3687fae335cd136092342eb6bd
DA-MO-WANG <zard20201116@gmail.com>
2022-11-10 17:36:00 +0800

    autosave leet

--- cpp/acwing/junior/shudefanwei.cpp
+++ cpp/acwing/junior/shudefanwei.cpp
@@ -10,7 +10,7 @@ using namespace std;
 const int N = 1e5 + 10;
 int q[N], query, n;
 
-void 
+
 
 int main() {
     //处理输入
@@ -23,6 +23,26 @@ int main() {
     for (int j = 0; j < query; ++j) {
         int x;
         scanf("%d",&x);
+        int l = 0, r = n - 1;
+        //把lr区间内所有元素跑一遍
+
+        //寻找<=x -last
+        while(l < r) {
+            int mid = (l + r) >> 1;
+            if(q[mid] <= x) {
+                l = mid;
+            }else r = mid - 1;
+        }
+        if(q[l] != x) cout << "-1 -1";
+        else {
+            cout << l << " ";
+            int l1 = 0, r1 = n - 1;
+            while(l1 < r1) {
+                int mid
+            }
+        }
+
+
 
     }
     return 0;
